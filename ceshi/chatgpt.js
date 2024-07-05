22d6589be60767a7b19db33e4a4c34beb0c22ff0
yjlsx <2413704955@qq.com>
2024-07-05 21:03:51 +0800

    .

---
+++ ceshi/chatgpt.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Template to compose HTTP reqeuest.
+ * 
+ */
+
+[rewrite_local]
+^https:\/\/ios\.chat\.openai\.com\/backend-api\/accounts\/check\/v4-2023-04-27$ script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/rewrite.js
+
+[mitm]
+hostname = ios.chat.openai.com
+
+
+// Quantumult X Rewrite Script
+const body = $response.body;
+let obj = JSON.parse(body);
+
+// Modify the JSON response for the default account
+obj.accounts.default.account.plan_type = "plus";
+obj.accounts.default.account.has_previously_paid_subscription = true;
+obj.accounts.default.is_eligible_for_yearly_plus_subscription = true;
+obj.accounts.default.entitlement.subscription_plan = "chatgptplusplan";
+obj.accounts.default.entitlement.has_active_subscription = true;
+obj.accounts.default.entitlement.expires_at = "2099-12-31T23:59:59Z";  // Setting a far future expiry date
+
+// Convert JSON object back to string
+const modifiedBody = JSON.stringify(obj);
+
+// Return the modified response
+$done({ body: modifiedBody });
+
