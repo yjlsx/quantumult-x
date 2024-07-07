1bd5b4c84e7433181c34b8163147fc67f1369b4b
yjlsx <2413704955@qq.com>
2024-07-08 07:04:32 +0800

    .

--- rewrite/spider.js
+++ rewrite/spider.js
@@ -37,8 +37,29 @@ try {
         // 修改 member/master 响应体
         jsonBody.data.vip_status = "ok"; // 设置为 VIP 状态
         jsonBody.data.is_master = "1";  
-        jsonBody.data.accredit_total = 1;
-        jsonBody.data.accredit_list = [" 688cb04816dd41c59ac73f252a7e52d9"];  
+        jsonBody.data.accredit_total = 4;
+        jsonBody.data.parking_total = 0;
+        jsonBody.data.accredit_list = [
+            {
+                "title": "抓包",
+                "content": "抓取 HTTP/HTTPS 流量"
+            },
+            {
+                "title": "重写",
+                "content": "实时修改 HTTP/HTTPS 的请求和响应"
+            },
+            {
+                "title": "数据共享",
+                "content": "多账户之间数据共享"
+            },
+            {
+                "title": "脚本",
+                "content": "支持自定义 JavaScript 语法进行接口的拓展编写"
+            },
+            {
+                "title": "重放",
+                "content": "支持接口依赖&断言的自定义 HTTP(S) 网络请求"
+            }];  
         jsonBody.data.vip_expiration_time = "2099-12-01";
         jsonBody.data.user_rights = [
             {
@@ -69,7 +90,8 @@ try {
         keyObj.vip_expiration_time = "2099-12-01"; // 设置 VIP 过期时间为 2099-12-01
         keyObj.vip_status = "ok";
         keyObj.createtime = 1695088494;
-        keyObj.accredit_total = 1;
+        keyObj.parking_total = 0;
+        keyObj.accredit_total = 4;
         keyObj.accredit_list = jsonBody.data.accredit_list;
         
         jsonBody.meta.key = JSON.stringify(keyObj);
