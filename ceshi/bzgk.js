e73e433e2908457150e87734e07a29e2daf09f8f
yjlsx <2413704955@qq.com>
2024-07-16 07:29:46 +0800

    .

--- ceshi/bzgk.js
+++ ceshi/bzgk.js
@@ -1,4 +1,4 @@
- /**
+/**
  * @fileoverview Quantumult X 脚本
  *
  [rewrite_local]
@@ -9,11 +9,9 @@
  ^http://api\.yaotia\.cn/api/v2/userCourse/sxy url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
  ^http://api\.yaotia\.cn/api/v2/goods/combo\?tag_id=0 url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
 
-*
  [mitm]
  hostname = api.yaotia.cn
  */
-
 
 // 用户信息接口脚本 yaotia.js
 const url = $request.url;
@@ -27,7 +25,6 @@ if (url.includes('/user/v1/login') || url.includes('/user/v1/getUserInfo')) {
     obj.data.vip_end_time = 4102415999; // 2099年12月31日 23:59:59 的时间戳
     obj.data.bz_money = 999999999;
 }
-
 
 if (url.includes('/api/v1/upgrade/index')) {
     // 升级信息接口
@@ -44,21 +41,21 @@ if (url.includes('/api/v2/goods/infoMaster')) {
     // 商品信息接口
     obj.data.is_goods_bought = 1;
     obj.data.need_buy = 0;
-    goodsObj.data.price = "0";
-    goodsObj.data.ori_price = "0";
+    obj.data.price = "0";
+    obj.data.ori_price = "0";
 }
 
 if (url.includes('/api/v2/userCourse/sxy')) {
     // 用户课程接口
-const newCourse = {
-    "id": 52,
-    "expire_tip": "",
-    "course_type": 96,
-    "is_expire": 0,
-    "cover": "https://img.yaotia.com/2023/10-13/1697161787037.png?size=188X224",
-    "name": "行测三板斧-风暴羚羊",
-    "desc": "课程有效期截至:2099-12-22"
-};
+    const newCourse = {
+        "id": 52,
+        "expire_tip": "",
+        "course_type": 96,
+        "is_expire": 0,
+        "cover": "https://img.yaotia.com/2023/10-13/1697161787037.png?size=188X224",
+        "name": "行测三板斧-风暴羚羊",
+        "desc": "课程有效期截至:2099-12-22"
+    };
     obj.data.list.push(newCourse);
 }
 
@@ -70,8 +67,7 @@ if (url.includes('/api/v1/order/wbuy')) {
 }
 
 if (url.includes('/api/v1/order/confirm')) {
-// 订单确认接口
-if (url.includes('/api/v1/order/confirm')) {
+    // 订单确认接口
     obj.data.not_need_money = 1;
     obj.data.total_amount = "0.00";
     obj.data.bz_money = "999999999";
@@ -84,8 +80,8 @@ if (url.includes('/api/v1/order/confirm')) {
     obj.data.gifts_list = [];
 }
 
-// 套餐购买状态接口
 if (url.includes('/api/v2/goods/combo?tag_id=0')) {
+    // 套餐购买状态接口
     obj.data.list.forEach(item => {
         item.is_buy = 1; // 将所有套餐的购买状态设为已购买
     });
