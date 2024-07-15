854b12c9f6cf6c733bd4a3896db14e7be7f66973
yjlsx <2413704955@qq.com>
2024-07-16 05:54:59 +0800

    .

--- ceshi/bzgk.js
+++ ceshi/bzgk.js
@@ -6,79 +6,81 @@
  ^http://api\.yaotia\.cn/api/v1/upgrade/index url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
  ^http://api\.yaotia\.cn/api/v1/order/wbuy url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
  ^http://api\.yaotia\.cn/api/v2/goods/infoMaster\?goods_id=52 url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
- *
+ ^http://api\.yaotia\.cn/api/v2/userCourse/sxy url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
+*
  [mitm]
  hostname = api.yaotia.cn
  */
 
-// 获取请求的 URL
-const url = $request.url;
 
-// 根据请求的 URL 区分处理逻辑
-if (url.includes("/user/v1/login") || url.includes("/user/v1/getUserInfo")) {
-    handleUserInfo();
-} else if (url.includes("/api/v1/upgrade/index")) {
-    handleUpgradeInfo();
-} else if (url.includes("/api/v1/order/wbuy")) {
-    handleOrderWbuy();
-} else if (url.includes("/api/v2/goods/infoMaster?goods_id=52")) {
-    handleGoodsInfo();
-}
+// 用户信息接口脚本 yaotia.js
+const userBody = $response.body;
+const userObj = JSON.parse(userBody);
 
-function handleUserInfo() {
-    const body = $response.body;
-    const obj = JSON.parse(body);
+// 修改 is_vip 字段为 1，表示用户是永久 VIP
+userObj.data.is_vip = 1;
+userObj.data.vip_txt = "永久VIP";
+userObj.data.vip_end_time = 4102415999; // 2099年12月31日 23:59:59 的时间戳
 
-    // 修改 is_vip 字段为 1，表示用户是永久 VIP
-    obj.data.is_vip = 1;
-    obj.data.vip_txt = "永久VIP";
-    obj.data.vip_end_time = 4102415999; // 2099年12月31日 23:59:59 的时间戳
+// 修改 bz_money 字段为很大的值
+userObj.data.bz_money = 999999999;
 
-    // 修改 bz_money 字段为很大的值
-    obj.data.bz_money = 999999999;
+$done({ body: JSON.stringify(userObj) });
 
-    $done({ body: JSON.stringify(obj) });
-}
+// 升级信息接口脚本 yaotia_upgrade.js
+const upgradeBody = $response.body;
+const upgradeObj = JSON.parse(upgradeBody);
 
-function handleUpgradeInfo() {
-    const body = $response.body;
-    const obj = JSON.parse(body);
+// 修改 VIP 相关字段
+upgradeObj.data.user.vip = 3;
+upgradeObj.data.user.vip_end_time = "2099-12-31";
+upgradeObj.data.user.desc = "永久VIP，尽情享受所有功能！";
 
-    // 修改 VIP 相关字段
-    obj.data.user.vip = 3;
-    obj.data.user.vip_end_time = "2099-12-31";
-    obj.data.user.desc = "永久VIP，尽情享受所有功能！";
+// 修改 VIP 权益列表
+upgradeObj.data.info.power.forEach(item => {
+    item.vip = true;
+});
 
-    // 修改 VIP 权益列表
-    obj.data.info.power.forEach(item => {
-        item.vip = true;
-    });
+// 修改 VIP 权益 URL
+upgradeObj.data.rights_url = "http://fed.midasjoy.com/Public/act/2021buzhi/vipequity/index.html?v=1";
 
-    // 修改 VIP 权益 URL
-    obj.data.rights_url = "http://fed.midasjoy.com/Public/act/2021buzhi/vipequity/index.html?v=1";
+$done({ body: JSON.stringify(upgradeObj) });
 
-    $done({ body: JSON.stringify(obj) });
-}
+// 商品信息接口脚本 yaotia_goods.js
+const goodsBody = $response.body;
+const goodsObj = JSON.parse(goodsBody);
 
-function handleOrderWbuy() {
-    const body = $response.body;
-    const obj = JSON.parse(body);
+// 修改商品信息为已购买状态
+goodsObj.data.is_goods_bought = 1;
+goodsObj.data.need_buy = 0;
 
-    // 修改 finish 字段为 1，表示订单已完成
-    obj.data.finish = 1;
+$done({ body: JSON.stringify(goodsObj) });
 
-    $done({ body: JSON.stringify(obj) });
-}
+// 用户课程接口脚本 yaotia_courses.js
+const coursesBody = $response.body;
+const coursesObj = JSON.parse(coursesBody);
 
-function handleGoodsInfo() {
-    const body = $response.body;
-    const obj = JSON.parse(body);
+// 新增课程信息到列表中
+const newCourse = {
+    "id": 52,
+    "expire_tip": "",
+    "course_type": 5,
+    "is_expire": 0,
+    "cover": "https://img.yaotia.com/2023/10-13/1697161787037.png?size=188X224",
+    "name": "行测三板斧-风暴羚羊",
+    "desc": "课程有效期截至:2099-12-22"
+};
 
-    // 修改已购买状态
-    obj.data.is_goods_bought = 1;
+coursesObj.data.list.push(newCourse);
 
-    // 修改 need_buy 为 0，表示无需购买
-    obj.data.need_buy = 0;
+$done({ body: JSON.stringify(coursesObj) });
 
-    $done({ body: JSON.stringify(obj) });
-}
+// 订单接口脚本 yaotia_order.js
+const orderBody = $response.body;
+const orderObj = JSON.parse(orderBody);
+
+// 修改订单完成状态为已完成
+orderObj.data.finish = 1;
+
+$done({ body: JSON.stringify(orderObj) });
+
