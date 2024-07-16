5f27910104a4ac2b67d1e79273ad4ca7992ee0db
yjlsx <2413704955@qq.com>
2024-07-16 08:34:14 +0800

    .

--- ceshi/bzgk.js
+++ ceshi/bzgk.js
@@ -10,9 +10,11 @@
  ^http://api\.yaotia\.cn/api/v2/goods/combo\?tag_id=0 url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
  ^http://api\.yaotia\.cn/api/v2/userCourse/recent url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/bzgk.js
 
+ *
  [mitm]
  hostname = api.yaotia.cn
  */
+
 
 // 用户信息接口脚本 yaotia.js
 const url = $request.url;
@@ -26,6 +28,7 @@ if (url.includes('/user/v1/login') || url.includes('/user/v1/getUserInfo')) {
     obj.data.vip_end_time = 4102415999; // 2099年12月31日 23:59:59 的时间戳
     obj.data.bz_money = 999999999;
 }
+
 
 if (url.includes('/api/v1/upgrade/index')) {
     // 升级信息接口
@@ -44,19 +47,20 @@ if (url.includes('/api/v2/goods/infoMaster')) {
     obj.data.need_buy = 0;
     obj.data.price = "0";
     obj.data.ori_price = "0";
+    obj.data.is_goods_bought = "1";
 }
 
 if (url.includes('/api/v2/userCourse/sxy')) {
     // 用户课程接口
-    const newCourse = {
-        "id": 76,
-        "expire_tip": "",
-        "course_type": 5,
-        "is_expire": 0,
-        "cover": "https://img.yaotia.com/2023/10-13/1697161787037.png?size=188X224",
-        "name": "行测三板斧-风暴羚羊",
-        "desc": "课程有效期截至:2099-12-22"
-    };
+const newCourse = {
+    "id": 76,
+    "expire_tip": "",
+    "course_type": 4,
+    "is_expire": 0,
+    "cover": "https://img.yaotia.com/2023/10-13/1697161787037.png?size=188X224",
+    "name": "行测三板斧-风暴羚羊",
+    "desc": "课程有效期截至:2099-12-22"
+};
     obj.data.list.push(newCourse);
 }
 
@@ -76,25 +80,35 @@ if (url.includes('/api/v1/order/confirm')) {
         goods.price = "0.00";
         goods.ori_price = "0.00";
     });
-    obj.data.failure_info = null; // 确保不显示支付失败信息
+    if (obj.data.failure_info) {
+        obj.data.failure_info = null;
+    }
     obj.data.original_price = "0.00";
     obj.data.gifts_list = [];
 }
 
+// 套餐购买状态接口
 if (url.includes('/api/v2/goods/combo?tag_id=0')) {
-    // 套餐购买状态接口
     obj.data.list.forEach(item => {
-        item.is_buy = 1; // 将所有套餐的购买状态设为已购买
+        item.is_buy = 1;
     });
 }
 
+// 添加大咖课
 if (url.includes('/api/v2/userCourse/recent')) {
-    // 最近课程接口
-    const newTab = {
+    obj.data.tabs.push({
         "tab_name": "大咖课",
-        "course_type": 5
-    };
-    obj.data.tabs.push(newTab); // 添加新的 tab
+        "course_type": 4
+    });
+    obj.data.list.push({
+        "yta_goods_type": 1,
+        "yta_goods_id": 52,
+        "subject": "大咖课",
+        "course_type": 4,
+        "course_cover": "https://img.yaotia.com/2023/10-13/1697161787037.png?size=188X224",
+        "course_name": "行测三板斧-风暴羚羊",
+        "course_id": 76
+    });
 }
 
 $done({ body: JSON.stringify(obj) });
