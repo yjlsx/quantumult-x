7269dddf5365c5749b01111e997c073f754f0f62
yjlsx <2413704955@qq.com>
2024-07-17 23:20:23 +0800

    .

--- ceshi/ywjy.js
+++ ceshi/ywjy.js
@@ -5,6 +5,7 @@
 ^https://proxyweb\.yiwenjy\.com/yiwen_mobile/query_classDetail_info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/ywjy.js
 ^https://proxyweb\.yiwenjy\.com/yiwen_mobile/query_myMessage\?clientId= url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/ywjy.js
 ^https://proxyweb\.yiwenjy\.com/yiwen_mobile/my_moneybag url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/ywjy.js
+^https://proxyweb\.yiwenjy\.com/yiwen_mobile/ioscoin_buy url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/ywjy.js
 *
 [mitm]
 hostname = proxyweb.yiwenjy.com
@@ -37,8 +38,15 @@ if (url.includes("/yiwen_mobile/query_myMessage")) {
 if (url.indexOf("my_moneybag") !== -1) {
     // 修改我的钱包 balance 和 code 字段的值
     obj.data.balance = 999999;  // 修改为你想要的 balance 值
-    obj.coin = 999999;  // 修改为你想要的 coin 值
+    obj.data.coin = 999999;  // 修改为你想要的 coin 值
 }
+
+if (url.indexOf("ioscoin_buy") !== -1) {
+    var json = JSON.parse(body);
+    json.msg = "成功支付";
+    json.code = 0;
+} 
+
 
 $done({ body: JSON.stringify(obj) });
 
