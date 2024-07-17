b71c1fbaf4560ee17334415802afa2750858b09f
yjlsx <2413704955@qq.com>
2024-07-17 19:01:59 +0800

    .

--- ceshi/jdwzbk.js
+++ ceshi/jdwzbk.js
@@ -22,8 +22,7 @@ if (url.includes('/questionset/getInfo')) {
         obj.data.key = 'i7';
         obj.data.has = 4;
         obj.data.expire_time = "2099-12-31";
-        obj.data.usability_name = '永久会员';
-        obj.data.buy_number = [63, 64, 67]; // 设置为已购买状态
+        obj.data.buy_number = 3312; // 设置为已购买状态
     }
 }
 
@@ -48,22 +47,8 @@ if (url.includes('home/getData')) {
         for (let item of obj.data.qs) {
             if (item.key === 'sj4') {
                 item.buy_number = 3312; // 设置为已购买状态
-                item.key = "i7";
             }
         }
-    }
-}
-
-// 根据请求的 URL 不同执行不同的修改
-if (url.includes('/identity/getInfo') || url.includes('/identity/listData') || url.includes('/pay/getPayInfo') || url.includes('/questionset/listData')) {
-    if (obj && obj.data && Array.isArray(obj.data)) {
-        obj.data.forEach(item => {
-            if (item.key.startsWith('sj')) {
-                item.key = 'i7'; // 将 sj 替换为 i7
-            }
-            // 将会员可用改为永久会员
-            item.usability_name = "永久会员";
-        });
     }
 }
 
