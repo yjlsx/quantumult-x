fd18465fb86ea9f8bfcc25a333b6cd74fd1e06a3
yjlsx <2413704955@qq.com>
2024-07-17 14:03:37 +0800

    .

--- ceshi/fenbi.js
+++ ceshi/fenbi.js
@@ -1,16 +1,17 @@
 # Quantumult X Rewrite
 
 [rewrite_local]
-/**
-# 重写 AI 教师会员信息
+# 修改 AI 教师会员信息
 ^https:\/\/keapi\.fenbi\.com\/ai\/iphone\/entry url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fenbi.js
 # 修改用户余额为
 ^https:\/\/ke\.fenbi\.com\/iphone\/v3\/users\/balance url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fenbi.js
-#改价格
+# 修改课程价格
 ^https:\/\/ke\.fenbi\.com\/iphone\/v3\/member_centers\/sale_center url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fenbi.js
-^https:\/\/ke\.fenb\i.com\/iphone\/v3\/user_member\/home url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fenbi.js
-#会员显示
+# 修改会员显示
+^https:\/\/ke\.fenbi\.com\/iphone\/v3\/user_member\/home url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fenbi.js
+# 修改课程配置
 ^https:\/\/ke\.fenbi\.com\/iphone\/v3\/user_member\/course_configs url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fenbi.js
+
 
 *
 [mitm]
@@ -43,23 +44,22 @@ if (url.includes('/iphone/v3/users/balance')) {
     }
 }
 
-//课程价格修改
+// 课程价格修改
 if (url.includes('/iphone/v3/member_centers/sale_center')) {
-  // 修改 data 中的 memberContents 和 contents 的部分属性值
-  for (let item of obj.data) {
-    if (item.memberContents) {
-      for (let content of item.memberContents) {
-        content.payPrice = 0;
-        content.price = 0;
-      }
+    for (let item of obj.data) {
+        if (item.memberContents) {
+            for (let content of item.memberContents) {
+                content.payPrice = 0;
+                content.price = 0;
+            }
+        }
+        if (item.contents) {
+            for (let content of item.contents) {
+                content.payPrice = 0;
+                content.price = 0;
+            }
+        }
     }
-    if (item.contents) {
-      for (let content of item.contents) {
-        content.payPrice = 0;
-        content.price = 0;
-      }
-    }
-  }
 }
 
 // 判断 URL 是否包含特定地址
@@ -76,74 +76,57 @@ if (url.includes('/iphone/v3/user_member/home')) {
     }
 
     // 修改 memberConfig 部分
-    if (obj.data && obj.data.memberConfig) {
-        obj.data.memberConfig.memberType = [1, 2, 4, 5, 7, 8, 9, 10, 11, 13, 14, 16, 17, 18, 20, 40, 52]; // 设置会员配置的类型为指定数组
-    }
+  if (obj.data && obj.data.memberConfig) {
+        obj.data.memberConfig.memberType = [1, 2, 4, 5, 7, 8, 9, 10, 11, 13, 14, 16, 17, 18, 20, 40, 52];
+        obj.data.memberConfig.shadowColor = "111111";
+        obj.data.memberConfig.textColor = "efb96d";
 
-// 修改 memberConfig 的 shadowColor 和 textColor
-            if (obj.data.memberConfig) {
-                obj.data.memberConfig.shadowColor = "111111";
-                obj.data.memberConfig.textColor = "efb96d";
-            }
-
-if (obj && obj.data && obj.data.memberConfig && Array.isArray(obj.data.memberConfig.memberBenefits)) {
+        if (Array.isArray(obj.data.memberConfig.memberBenefits)) {
             obj.data.memberConfig.memberBenefits.forEach(benefit => {
-                // 修改视频解析和精选电子书的 native 为 false
                 if (benefit.title === "视频解析" || benefit.title === "精选电子书") {
                     benefit.native = false;
                 }
             });
-  }
+        }
+    }
 
-// 将所有电子书设为已购买
-if (obj.data && obj.data.modules) {
-    for (let module of obj.data.modules) {
-        if (module.memberModuleType === 1 && module.content.eBooks) {
-            for (let ebook of module.content.eBooks) {
-                ebook.paid = true; // 将电子书标记为已购买
+// 将所有电子书//课程设为已购买
+ if (obj.data && obj.data.modules) {
+        for (let module of obj.data.modules) {
+            if (module.memberModuleType === 1 && module.content.eBooks) {
+                for (let ebook of module.content.eBooks) {
+                    ebook.paid = true;
+                }
+            }
+            if (module.memberModuleType === 4 && module.content.lectures) {
+                for (let lecture of module.content.lectures) {
+                    lecture.hasAudition = true;
+                }
+            }
+            if (module.memberModuleType === 2 && module.content.userMorningReading) {
+                module.content.userMorningReading.trial = false;
+                module.content.userMorningReading.memberUserSummaryRet.readMemberArticle = true;
             }
         }
     }
-}
 
-// 将所有课程设为已购买
-if (obj.data && obj.data.modules) {
-    for (let module of obj.data.modules) {
-        if (module.memberModuleType === 4 && module.content.lectures) {
-            for (let lecture of module.content.lectures) {
-                lecture.hasAudition = true; // 将课程标记为已试听，相当于已购买
-            }
-        }
-    }
-}
-
-// 将所有会员专享内容设为已购买
-if (obj.data && obj.data.modules) {
-    for (let module of obj.data.modules) {
-        if (module.memberModuleType === 2 && module.content.userMorningReading) {
-            module.content.userMorningReading.trial = false; // 取消试用状态
-            module.content.userMorningReading.memberUserSummaryRet.readMemberArticle = true; // 标记会员专享内容已读
-        }
-    }
-}
 // 将所有已购买
-obj.data.modules.forEach(module => {
-    if (module.content) {
-        if (module.content.eBooks) {
-            module.content.eBooks.forEach(eBook => {
-                eBook.paid = true;
-                eBook.free = true;
-            });
+    obj.data.modules.forEach(module => {
+        if (module.content) {
+            if (module.content.eBooks) {
+                module.content.eBooks.forEach(eBook => {
+                    eBook.paid = true;
+                    eBook.free = true;
+                });
+            }
+            if (module.content.lectures) {
+                module.content.lectures.forEach(lecture => {
+                    lecture.paid = true;
+                    lecture.free = true;
+                });
+            }
         }
-        if (module.content.lectures) {
-            module.content.lectures.forEach(lecture => {
-                lecture.paid = true;
-                lecture.free = true;
-            });
-        }
-      }
-  });
-}
+    });
 
 // 修改每个都为SVIP
     if (url.includes("ke.fenbi.com/iphone/v3/user_member/course_configs")) {
@@ -156,9 +139,8 @@ obj.data.modules.forEach(module => {
                     });
                 }
             });
-          }
+        }
+    }
 }
-
-
 
 $done({body: JSON.stringify(obj)});
