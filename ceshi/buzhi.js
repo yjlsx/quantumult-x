20109c3e2f5bb651aaa32e2a16326219803c135c
yjlsx <2413704955@qq.com>
2024-07-19 04:50:10 +0800

    .

---
+++ ceshi/buzhi.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ [rewrite_local]
+^http://api\.yaotia\.cn/api/v1/course/sprintInfo\?course_id=76 url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/buzhi.js
+*
+ [mitm]
+ hostname = api.yaotia.cn
+ */
+
+const dataUrl = 'https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fbly.json'; 
+// 判断请求 URL
+if (url.includes('http://api.yaotia.cn/api/v1/course/sprintInfo?course_id=76')) {
+  // 获取 `` 文件的内容
+  $http.get(dataUrl).then(response => {
+    $response.body = response.body;
+    $done();
+  }).catch(() => {
+    // 如果请求 失败，返回错误信息
+    $response.body = JSON.stringify({ error: 'Failed to fetch fbly.json' });
+    $done();
+  });
+} else {
+  // 如果 URL 不匹配，则不做任何修改
+  $done();
+}
