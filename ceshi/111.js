b143fe9bbd2bac7c3e484a1787b100f151a7a417
yjlsx <2413704955@qq.com>
2024-07-19 16:55:11 +0800

    .

--- ceshi/111.js
+++ ceshi/111.js
@@ -10,13 +10,17 @@
 const requestUrl = 'http://api.yaotia.cn/api/v1/course/sprintInfo?course_id=76'; 
 
 const newFields = [
-          {
+                   {
+             "service_url" : "http://fed.midasjoy.com/Public/act/2021act/sprintTeacher/index.html?cid=29",
+             "is_service" : 0,
+            "stage_list" : [
+           {
             "id" : 29,
             "instruct" : "第一阶段",
             "chapter_name" : "夯实基础：羚羊行测123卷带练课",
-            "video_num" : 1,
+            "video_num" : 2,
             "stage_id" : 0,
-            "node_num" : 0,
+            "node_num" : 1,
             "desc" : "共1节课程",
             "node_list" : [
               {
@@ -34,12 +38,12 @@ const newFields = [
                   {
                     "is_finish" : 0,
                     "channel_id" : 1,
-                    "live_status" : 3,
+                    "live_status" : 4,
                     "title" : "观看课程",
                     "live_id" : 0,
                     "is_vid_edited" : 1,
                     "video_id" : "1e6eaa05afc3757f316558e95016f444_1",
-                    "type" : 3,
+                    "type" : 4,
                     "orientation" : 1,
                     "bl_channel_id" : 202956
                   }
@@ -67,12 +71,12 @@ const newFields = [
                   {
                     "is_finish" : 0,
                     "channel_id" : 1,
-                    "live_status" : 3,
+                    "live_status" : 4,
                     "title" : "观看课程",
                     "live_id" : 0,
                     "is_vid_edited" : 1,
                     "video_id" : "1e6eaa05afaed9fce49785ad828ddff0_1",
-                    "type" : 3,
+                    "type" : 4,
                     "orientation" : 1,
                     "bl_channel_id" : 202956
                   }
@@ -84,10 +88,29 @@ const newFields = [
                 "ti_type_name" : "行测",
                 "node_name" : "2.2 003卷：如何正确判断选项（新）",
                 "ti_type" : 1
-              },
+              }
 
 
 
+
+            ]
+          }
+        ]
+      }
+    ],
+    "course_type" : 4,
+    "is_resource" : 0,
+    "progress" : {
+      "std_show" : 1,
+      "work_show" : 1,
+      "std_progress" : 0,
+      "work_progress" : 0
+    },
+    "course_name" : "行测三板斧·风暴羚羊",
+    "course_today" : [
+
+    ]
+  }
 ];
 
 if ($request.url.includes(requestUrl)) {
@@ -96,7 +119,7 @@ if ($request.url.includes(requestUrl)) {
   let responseJson = JSON.parse(body);
 
   // 替换 data 数组
-  responseJson.data.chapter_list = newFields;
+  responseJson.data = newFields;
 
   // 返回修改后的响应体
   $done({ body: JSON.stringify(responseJson) });
