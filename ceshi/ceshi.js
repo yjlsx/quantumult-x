05f0369823e1f1c1dcf73394f4df040b60cfa813
yjlsx <2413704955@qq.com>
2024-07-20 09:47:18 +0800

    .

--- ceshi/ceshi.js
+++ ceshi/ceshi.js
@@ -1,11 +1,11 @@
 // Quantumult X rewrite script
-
+/*
 [rewrite_local]
 ^https:\/\/docer\.wps\.cn\/v3\.php\/api\/ios\/mobile\/v1\/coupon\?status=unused url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/ceshi.js
 
 [mitm]
 hostname = docer.wps.cn
-
+*/
 
 let body = $response.body;
 let obj = JSON.parse(body);
