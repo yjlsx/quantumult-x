3b3f749e22dd2f7a25716f2dfe4d6ca11ead1610
yjlsx <2413704955@qq.com>
2024-07-20 09:44:49 +0800

    .

---
+++ ceshi/ceshi.js
@@ -0,0 +1,16 @@
+// Quantumult X rewrite script
+
+[rewrite_local]
+^https:\/\/docer\.wps\.cn\/v3\.php\/api\/ios\/mobile\/v1\/coupon\?status=unused script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/ceshi.js
+
+[mitm]
+hostname = docer.wps.cn
+
+
+let body = $response.body;
+let obj = JSON.parse(body);
+
+obj.data.total = 999999;
+
+body = JSON.stringify(obj);
+$done({body});
