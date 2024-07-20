6fc856ba898eda9928807e64500e4fc164122065
yjlsx <2413704955@qq.com>
2024-07-20 12:16:59 +0800

    .

--- ceshi/fanyi.js
+++ ceshi/fanyi.js
@@ -5,6 +5,7 @@
 ^https:\/\/docer-api\.wps\.cn\/proxy\/docer\/v3\.php\/api\/ios\/mobile\/v1\/coupon url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js
 ^https:\/\/pay\.wps\.cn\/api\/pay\/notify\/couponpay url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js
 ^https:\/\/vip\.wps\.cn\/pay_config\/v1\/config\/member\?csource=ios_coupons_doc_translate&payconfig=doc_translate_ios&struct_type=type url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js 
+^https:\/\/tiance\.wps\.cn\/dce\/exec\/api\/market\/activity url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js
 
 [mitm]
 hostname = docer.wps.cn, docer-api.wps.cn
@@ -13,16 +14,16 @@ hostname = docer.wps.cn, docer-api.wps.cn
 let body = $response.body;
 let obj = JSON.parse(body);
 
-// Check if the response is from the coupon API
+// Check if the response is from the coupon API and modify 'total'
 if (obj.data && obj.data.total !== undefined) {
-  obj.data.total = 999999;
+  obj.data.total = 899999;
 } 
-// Check if the response is from the coupon pay API
+// Check if the response is from the coupon pay API and modify 'result' and 'msg'
 else if (obj.result && obj.msg) {
   obj.result = "ok";
   obj.msg = "使用优惠券成功";
 } 
-// Check if the response contains the specific fields indicating the product or service detail
+// Check if the response is from the product or service detail API and modify 'enable_coupon'
 else if (obj.data && obj.data.type) {
   obj.data.type.forEach(item => {
     if (item.id === 400008 && item.payunit === "页") {
@@ -32,6 +33,11 @@ else if (obj.data && obj.data.type) {
   });
 }
 
+// Check if the response has a specific structure indicating an error message and modify 'result', 'code', and 'msg'
+if (obj.result === "ok" && obj.code === 5005003 && obj.msg === "用户不属于任何人群活动") {
+  obj.code = 0;
+  obj.msg = "用户属于人群活动";
+}
+
 body = JSON.stringify(obj);
 $done({body});
-
