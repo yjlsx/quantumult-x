7449fcac6d4ded5dee8dc41968d95a56e41df533
yjlsx <2413704955@qq.com>
2024-07-20 13:38:41 +0800

    .

--- ceshi/fanyi.js
+++ ceshi/fanyi.js
@@ -7,9 +7,10 @@
 ^https:\/\/vip\.wps\.cn\/pay_config\/v1\/config\/member\?csource=ios_coupons_doc_translate&payconfig=doc_translate_ios&struct_type=type url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js 
 ^https:\/\/tiance\.wps\.cn\/dce\/exec\/api\/market\/activity url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js
 ^https:\/\/account\.wps\.cn\/api\/v3\/islogin url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js
+^https:\/\/softbus-device\.wps\.cn\/api\/v1\/device\/list url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyi.js
 
 [mitm]
-hostname = docer.wps.cn, docer-api.wps.cn, tiance.wps.cn
+hostname = docer.wps.cn, docer-api.wps.cn, tiance.wps.cn,softbus-device.wps.cn
 */
 let body = $response.body;
 let obj = JSON.parse(body);
@@ -43,49 +44,21 @@ if (url.includes('/v3.php/api/ios/mobile/v1/coupon')) {
     obj.current_companyid = 999999;
   }
 }else if (url.includes('/api/pay/notify/couponpay')) {
+  obj.result = ok;
   obj.msg = "success";
-}
-
-
-
-body = JSON.stringify(obj);
-$done({body});
-
-
-
-
-
-
-
-
-
-let body = $response.body;
-let obj = JSON.parse(body);
-
-// Check if the response is from the coupon API and modify 'total'
-if (obj.data && obj.data.total !== undefined) {
-  obj.data.total = 999999;
-} 
-// Check if the response is from the coupon pay API and modify 'result' and 'msg'
-else if (obj.result && obj.msg) {
-  obj.result = "ok";
-  obj.msg = "使用优惠券成功";
-} 
-// Check if the response is from the product or service detail API and modify 'enable_coupon'
-else if (obj.data && obj.data.type) {
-  obj.data.type.forEach(item => {
-    if (item.id === 400008 && item.payunit === "页") {
-      // Modify the necessary fields
-      item.enable_coupon = 999999;
+}else if (url.includes('/api/v1/device/list')) {
+// 修改 transfer_helper 为 1
+  obj.data.devices.forEach(device => {
+    if (device.additional_info && device.additional_info.allow_notifies) {
+      device.additional_info.allow_notifies.transfer_helper = 1;
+    }
+    if (device.x_report) {
+      device.x_report.is_mark_chan = 1;
     }
   });
 }
 
-// Check if the response has a specific structure indicating an error message and modify 'result', 'code', and 'msg'
-if (obj.result === "ok" && obj.code === 5005003 && obj.msg === "用户不属于任何人群活动") {
-  obj.code = 0;
-  obj.msg = "用户属于人群活动";
-}
+
 
 body = JSON.stringify(obj);
 $done({body});
