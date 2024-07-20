bce76c62acf3ec9b40f65ad0c98644c027b4118c
yjlsx <2413704955@qq.com>
2024-07-20 16:29:59 +0800

    .

--- ceshi/fanyigou.js
+++ ceshi/fanyigou.js
@@ -6,6 +6,7 @@
 ^https:\/\/www\.fanyigou\.com\/payment\/vipTypeSpecial\/getIosList url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyigou.js
 ^https:\/\/www\.fanyigou\.com\/sdoc\/web\/getMyTranslate url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyigou.js
 ^https:\/\/www\.fanyigou\.com\/payment\/iosPay\/afterBuyTimeChick url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyigou.js
+^https:\/\/www\.fanyigou\.com\/qxscore\/trans\/getComputeInfo url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/fanyigou.js
 
 [mitm]
 hostname = www.fanyigou.com
@@ -35,10 +36,10 @@ if (url.includes('users/userInfoNew/app/getNewIndexInfo')) {
     obj.data.userScoreDetail.latelyTempScore = 9999;
     obj.data.userScoreDetail.latelyDays = 35;
     obj.data.userScoreDetail.vipPage = 999999;
-    obj.data.userScoreDetail.vipPageTime = "2099-12-31";
-    obj.data.userScoreDetail.permanentScore = 9999;
-    obj.data.userScoreDetail.vipScore = 9999;
-    obj.data.userScoreDetail.score = 9999;
+    obj.data.userScoreDetail.vipPageTime = 999999;
+    obj.data.userScoreDetail.permanentScore = 99999;
+    obj.data.userScoreDetail.vipScore = 99999;
+    obj.data.userScoreDetail.score = 99999;
     obj.data.memberCoin = 999999;
     obj.data.memberType.expireTime = "2099-12-31";
     obj.data.memberType.memberType = "vip";
@@ -67,6 +68,20 @@ if (url.includes('/payment/iosPay/afterBuyTimeChick')) {
    obj.code = 0;
    obj.ok = true;
    obj.msg = "请求成功";
+}
+if (url.includes('/qxscore/trans/getComputeInfo')) {
+   obj.data.computeScoreVo.freePageCount = 99999;
+   obj.data.computeScoreVo.isEnough = 1;
+   obj.data.computeScoreVo.payStatus = 1;
+   obj.data.computeScoreVo.payCount = 0;
+   obj.data.computePageVo.freePageCount =999999;
+   obj.data.computePageVo.isEnough = 1;
+   obj.data.computePageVo.payStatus = 1;
+   obj.data.computePageVo.payCount = 0;
+   obj.data.scoreAccountInfoVo.isVipUser = 1;
+   obj.data.scoreAccountInfoVo.score = 99999;
+   obj.data.scoreAccountInfoVo.specailPage = 999999;
+   obj.data.scoreAccountInfoVo.page = 9999;
 }
 
 
