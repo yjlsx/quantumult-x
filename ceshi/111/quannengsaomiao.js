5027b7533180d6149de0ae381104c81445756a39
yjlsx <2413704955@qq.com>
2024-07-21 17:08:36 +0800

    .

---
+++ ceshi/111/quannengsaomiao.js
@@ -0,0 +1,39 @@
+// Quantumult X rewrite script
+/*
+[rewrite_local]
+^https:\/\/api-cs\.intsig\.net\/purchase\/cs\/query_property\?app_type=CamScanner_IP_FREE&client_app=CamScanner_IP_FREE.*$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/quannengsaomiao.js
+
+
+[mitm]
+hostname = api-cs.intsig.net
+*/
+
+let body = $response.body;
+let obj = JSON.parse(body);
+
+// 获取请求 URL
+let url = $request.url;
+if (url.includes('/query_property?app_type')) {
+     obj.data.points = "999999";
+     obj.data.pdfword_balance = "999999";
+     obj.data.points_exchange_cfgrs.CamScanner_ID_Card_Credit = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Pdf2ppt = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Sign = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_ID_Card_Authenticity = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Excel = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Watermarks = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Profile_Card_Format = 0
+     obj.data.points_exchange_cfgrs.CamScanner_CertMode = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Pdf2excel = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Pdfword = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_CloudCap_1G = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_CloudOCR = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_Translation = 0;
+     obj.data.points_exchange_cfgrs.CamScanner_AlbumImport = 0;
+}
+
+
+body = JSON.stringify(obj);
+$done({body});
+
+
---
+++ ceshi/111/wps.js
@@ -0,0 +1,67 @@
+// Quantumult X rewrite script
+/*
+[rewrite_local]
+^https:\/\/docer\.wps\.cn\/v3\.php\/api\/ios\/mobile\/v1\/coupon\?status=unused url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wps.js
+^https:\/\/docer-api\.wps\.cn\/proxy\/docer\/v3\.php\/api\/ios\/mobile\/v1\/coupon url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wps.js
+^https:\/\/pay\.wps\.cn\/api\/pay\/notify\/couponpay url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wps.js
+^https:\/\/vip\.wps\.cn\/pay_config\/v1\/config\/member\?csource=ios_coupons_doc_translate&payconfig=doc_translate_ios&struct_type=type url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wps.js
+^https:\/\/tiance\.wps\.cn\/dce\/exec\/api\/market\/activity url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wps.js
+^https:\/\/account\.wps\.cn\/api\/v3\/islogin url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wps.js
+^https:\/\/softbus-device\.wps\.cn\/api\/v1\/device\/list url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wps.js
+
+[mitm]
+hostname = docer.wps.cn, docer-api.wps.cn, tiance.wps.cn,softbus-device.wps.cn, pay.wps.cn
+*/
+let body = $response.body;
+let obj = JSON.parse(body);
+
+// 获取请求 URL
+let url = $request.url;
+
+// 根据不同的 URL 修改响应体内容
+if (url.includes('/v3.php/api/ios/mobile/v1/coupon')) {
+  if (obj.data && obj.data.total !== undefined) {
+    obj.data.total = 999999;
+  }
+} else if (url.includes('/dce/exec/api/market/activity')) {
+  if (obj.result && obj.msg) {
+    obj.result = "ok";
+    obj.code = 0;
+    obj.msg = "使用优惠券成功";
+  }
+} else if (url.includes('/pay_config/v1/config/member')) {
+  if (obj.data && obj.data.type) {
+    obj.data.type.forEach(item => {
+      if (item.id === 400008 && item.payunit === "页") {
+        item.enable_coupon = [10, 20, 30, 50, 75, 100, 150, 1000];
+      }
+    });
+  }
+} else if (url.includes('/api/v3/islogin')) { // 替换为实际的路径
+  if (obj.companyid !== undefined) {
+    obj.is_company_account = false;
+    obj.companyid = 999999; // 填入正确的值
+    obj.current_companyid = 999999; // 填入正确的值
+  }
+} else if (url.includes('/api/pay/notify/couponpay')) {
+  if (obj.result && obj.msg) {
+    obj.result = "ok";
+    obj.msg = "使用优惠券成功";
+  }
+} else if (url.includes('/api/v1/device/list')) {
+  // 修改 transfer_helper 为 1 和 dsc_version 为 1
+  if (obj.data && obj.data.devices) {
+    obj.data.devices.forEach(device => {
+      if (device.additional_info && device.additional_info.allow_notifies) {
+        device.additional_info.allow_notifies.transfer_helper = 1;
+        device.additional_info.dsc_version = 1; // 这里的值需要根据实际需求填充
+      }
+      if (device.x_report) {
+        device.x_report.is_mark_chan = 1;
+      }
+    });
+  }
+}
+
+body = JSON.stringify(obj);
+$done({body});
