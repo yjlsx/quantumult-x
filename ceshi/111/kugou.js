a5f4168433db378b9e7087f22d76f7a1f19eb327
yjlsx <2413704955@qq.com>
2024-07-25 04:44:51 +0800

    .

--- ceshi/111/kugou.js
+++ ceshi/111/kugou.js
@@ -7,6 +7,7 @@
 ^https:\/\/vipuser\.kugou\.com url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kugou.js
 ^http:\/\/login\.user\.kugou\.com url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kugou.js
 ^https:\/\/gateway3\.kugou\.com url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kugou.js
+^https:\/\/vipos\.kugou\.com url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kugou.js
 *
 [mitm]
 hostname = gateway.kugou.com, gateway3.kugou.com, vip.kugou.com, vipuser.kygou.com, login.user.kugou.com, userinfoservice.kugou.com
@@ -28,25 +29,31 @@ body = body.replace(/("annual_fee_end_time" : )(".*?"|)(,|})/g, '$1"2022-03-17 0
 body = body.replace(/("bookvip_end_time" : )(".*?"|)(,|})/g, '$1"2022-03-17 09:05:55"$3');
 body = body.replace(/("is_special_vip" : )\d+|"is_special_vip" : ""/g, '$1 1');
 body = body.replace(/("is_free_vip" : )\d+|"is_free_vip" : ""/g, '$1 1');
-body = body.replace(/("is_vip" : )\d+|"is_vip" : ""/g, '$1 1');
 body = body.replace(/("vip_switch" : )\d+|"vip_switch" : ""/g, '$1 1');
 body = body.replace(/("vip_type" : )\d+|"vip_type" : ""/g, '$1 1');
-body = body.replace(/("svip_score" : )\d+|"svip_score" : ""/g, '$1 999999');
-body = body.replace(/("svip_level" : )\d+|"svip_level" : ""/g, '$1 8');
 body = body.replace(/("su_vip_upgrade_days" : )\d+|"su_vip_upgrade_days" : ""/g, '$1 999999');
 body = body.replace(/"h_signed" : " "/g, '"h_signed" : "ssvip"');
-body = body.replace(/("y_type" : )\d+|"y_type" : ""/g, '$1 1');
-body = body.replace(/("m_type" : )\d+|"m_type" : ""/g, '$1 1');
-body = body.replace(/("user_type" : )\d+|"user_type" : ""/g, '$1 1');
-body = body.replace(/("svip_level" : )\d+|"svip_level" : ""/g, '$1 8');
-body = body.replace(/("vip_statu" : )\d+|"vip_statu" : ""/g, '$1 1');
 body = body.replace(/("svip_left_days" : )\d+|"svip_left_days" : ""/g, '$1 999999');
 body = body.replace(/("t_expire_time" : )\d+|"t_expire_time" : ""/g, '$1 4102444800');
 body = body.replace(/"end_time" : \d+/g, '"end_time" : 4102444800');
 body = body.replace(/("multiplatform_vip_expire_prompt_myinfo_config_0" : "\{[^}]*"value":)\d+/, '$1 9999');
 body = body.replace(/("multiplatform_vip_text_vip_link_vipexpire" : )\[[^\]]*\]/g, '$1["VIP会员"]');
+// 替换 vipinfo 对象内各字段的值为 1，假设字段名是唯一的且不重复
+body = body.replace(/("is_vip" : )\d+/g, '$1 1');
+body = body.replace(/("y_type" : )\d+/g, '$1 1');
+body = body.replace(/("vip_type" : )\d+/g, '$1 1');
+body = body.replace(/("m_type" : )\d+/g, '$1 1');
+body = body.replace(/("svip_level" : )\d+/g, '$1 8');
+body = body.replace(/("svip_score" : )\d+/g, '$1 99999');
+body = body.replace(/("vip_statu" : )\d+/g, '$1 1');
+body = body.replace(/("user_type" : )\d+/g, '$1 1');
+body = body.replace(/("user_y_type" : )\d+/g, '$1 1');
 
-
-
+// 替换空字符串情况
+body = body.replace(/("vip_type" : "")/g, '"vip_type" : 1');
+body = body.replace(/("m_type" : "")/g, '"m_type" : 1');
+body = body.replace(/("originalPrice" : )\d+|"originalPrice" : ""/g, '$1 0');
+body = body.replace(/("price" : )\d+|"price" : ""/g, '$1 0');
+body = body.replace(/("price_text" : )\d+|"price_text" : ""/g, '$1 0');
 $done({body: body});
 
