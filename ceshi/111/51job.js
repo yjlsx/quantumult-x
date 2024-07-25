5c0e6ddb8e99db61b18dcc3ee9bd624118010f58
yjlsx <2413704955@qq.com>
2024-07-26 01:26:32 +0800

    .

--- ceshi/111/51job.js
+++ ceshi/111/51job.js
@@ -18,6 +18,13 @@ body = body.replace(/"price" : \d+/g, '"price" : 0');
 body = body.replace(/"rice" : \d+/g, '"price" : 0');
 body = body.replace(/("ihasCompetitivenessService" : )false/g, '$1true');
 body = body.replace(/("remainCompetitivenessCount" : )false/g, '$1true');
+body = body.replace(/"maxViewedCount" : 0/g, '"maxViewedCount" : 9999');
+body = body.replace(/"maxRefreshCount" : 0/g, '"maxRefreshCount" : 9999');
+body = body.replace(/"maxResumeCount" : 0/g, '"maxResumeCount" : 9999');
+body = body.replace(/"showManagementyRecruit" : 1/g, '"showManagementyRecruit" : 0');
+// 在 vipInfoVO 中添加 effectiveDate 字段并设置值
+body = body.replace(/("vipInfoVO" : \{)/, '$1"effectiveDate" : "2099-12-31T23:59:59Z", ');
+
 console.log("Modified Response Body: ", body);
 
 $done({body: body});
