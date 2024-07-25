287e496dd562e8ad3ebcde27125964fc0d6f5e1b
yjlsx <2413704955@qq.com>
2024-07-25 23:58:08 +0800

    .

--- ceshi/111/51job.js
+++ ceshi/111/51job.js
@@ -8,12 +8,16 @@
 hostname = cupid.51jobapp.com
 */
 var body = $response.body;
+console.log("Original Response Body: ", body);
+
 // 替换 isVip 字段的值为 true
 body = body.replace(/("isVip" : )false/g, '$1true');
 body = body.replace(/("showManagementPage" : )false/g, '$1true');
 body = body.replace(/"isfree" : 0/g, '"isfree" : 1');
 body = body.replace(/"price" : \d+/g, '"price" : 0');
 body = body.replace(/"rice" : \d+/g, '"price" : 0');
+body = body.replace(/("ihasCompetitivenessService" : )false/g, '$1true');
+body = body.replace(/("remainCompetitivenessCount" : )false/g, '$1true');
 console.log("Modified Response Body: ", body);
 
 $done({body: body});
