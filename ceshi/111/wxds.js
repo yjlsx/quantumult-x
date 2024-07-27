72fcc8d2c48a2aac9a18b248dce59933d4e95d4e
yjlsx <2413704955@qq.com>
2024-07-27 14:38:20 +0800

    .

--- ceshi/111/wxds.js
+++ ceshi/111/wxds.js
@@ -26,17 +26,19 @@ const url = $request.url;
 const body = $response.body;
 let obj = JSON.parse(body);
 
-if (url.includes('/pay/memberCardSummary')) {  
+if (url.includes('/pay/memberCardSummary')) {
     obj.canUseDiscount = 1;
     obj.remainCount = 9999;
     obj.startTime = 1711011078; // 你可以根据需要调整
     obj.totalFreeReadDay = 9999;
-    obj.remainCoupon = 55; 
-    obj.autoRenewableType = "permanent"; //自动续费类型
-    obj.hintsForRecharge.buttonSubtitle = "永久会员";
-    obj.hintsForRecharge.predictedChapterPrice = 0;
-    obj.hintsForRecharge.pricePerMonth = 0;
-    obj.giftIsExpired = 0; 
+    obj.remainCoupon = 55;
+    obj.autoRenewableType = "permanent"; // 自动续费类型
+    if (obj.hintsForRecharge) {
+        obj.hintsForRecharge.buttonSubtitle = "永久会员";
+        obj.hintsForRecharge.predictedChapterPrice = 0;
+        obj.hintsForRecharge.pricePerMonth = 0;
+    }
+    obj.giftIsExpired = 0;
     obj.tipforpayFlag = "0|0|0";
     obj.mcardHint = "永久会员";
     obj.payingRemainTime = 99999;
@@ -50,40 +52,44 @@ if (url.includes('/pay/memberCardSummary')) {
 }
 
 if (url.includes('/weekly/exchange')) {
-     if (obj.memberCardExchange) {
-    obj.memberCardExchange.forEach(card => {
-    card.canExchange = 1; 
-    card.days = 9999;  
-    card.price = 0;
-    card.needFreeCardCnt = 1;
-       });
-   }
-    obj.isMCardVip = 1;  // 设置为会员
-    obj.infiniteCard.day = 9999;
-    obj.infiniteCard.paying = 1;
+    if (obj.memberCardExchange) {
+        obj.memberCardExchange.forEach(card => {
+            card.canExchange = 1;
+            card.days = 9999;
+            card.price = 0;
+            card.needFreeCardCnt = 1;
+        });
+    }
+    if (obj.infiniteCard) {
+        obj.infiniteCard.day = 9999;
+        obj.infiniteCard.paying = 1;
+    }
+    obj.isMCardVip = 1; // 设置为会员
 }
 
 if (url.includes('/storyfeed/getRecentBooks')) {
-   if(obj.items) {
-   obj.items.forEach(function(item) {
-    item.book.free = 1; // 设置书籍为免费
-    item.book.originalPrice = 0; // 设置原价为 0
-    item.book.centPrice = 0;
-    item.book.payType = 0;    
-    });
+    if (obj.items) {
+        obj.items.forEach(function(item) {
+            if (item.book) {
+                item.book.free = 1; // 设置书籍为免费
+                item.book.originalPrice = 0; // 设置原价为 0
+                item.book.centPrice = 0;
+                item.book.payType = 0;
+            }
+        });
     }
 }
 
 if (url.includes('pay/balance')) {
-   obj.giftBalance = 9999;
-   obj.peerBalance = 999999;
-   obj.paperBalance = 999999;
-   obj.incentive = 9999;
-   obj.balance = 99999;
+    obj.giftBalance = 9999;
+    obj.peerBalance = 999999;
+    obj.paperBalance = 999999;
+    obj.incentive = 9999;
+    obj.balance = 99999;
 }
 
 if (url.includes('user/profile')) {
-   obj.role = 1;
+    obj.role = 1;
 }
 
 $done({ body: JSON.stringify(obj) });
