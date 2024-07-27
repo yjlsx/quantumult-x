83fbe963657a91cb22ef797344d701da38d2a997
yjlsx <2413704955@qq.com>
2024-07-27 14:28:24 +0800

    .

---
+++ ceshi/111/wxds.js
@@ -0,0 +1,89 @@
+/*
+ *
+ *
+脚本功能：微信读书 解锁付费vip
+下载地址：苹果商店下载
+脚本作者：
+更新时间：
+电报频道：
+问题反馈：
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+[rewrite_local]
+# > 微信读书vip(需重新用微信登陆，无法刷新->关闭脚本进入) 
+^https://i.\weread\.qq\.com/(login|pay/balance|user/profile).*$  url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
+^https://i\.weread\.qq\.com/pay/memberCardSummary url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
+^https://i.\weread\.qq\.com/weekly/exchange url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
+^https://i.\weread\.qq\.com/storyfeed/getRecentBooks  url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
+
+^https://i\.weread\.qq\.com/updateConfig url reject
+[mitm] 
+hostname = i.weread.qq.com
+*
+*
+*/
+const url = $request.url;
+const body = $response.body;
+let obj = JSON.parse(body);
+
+if (url.includes('/pay/memberCardSummary')) {  
+    obj.canUseDiscount = 1;
+    obj.remainCount = 9999;
+    obj.startTime = 1711011078; // 你可以根据需要调整
+    obj.totalFreeReadDay = 9999;
+    obj.remainCoupon = 55; 
+    obj.autoRenewableType = "permanent"; //自动续费类型
+    obj.hintsForRecharge.buttonSubtitle = "永久会员";
+    obj.hintsForRecharge.predictedChapterPrice = 0;
+    obj.hintsForRecharge.pricePerMonth = 0;
+    obj.giftIsExpired = 0; 
+    obj.tipforpayFlag = "0|0|0";
+    obj.mcardHint = "永久会员";
+    obj.payingRemainTime = 99999;
+    obj.shareForCardIsActive = 1; // 分享卡片状态为激活
+    obj.payingUsedDay = 1;
+    obj.permanent = 1; // 标记为永久会员
+    obj.expiredTime = 4102444799; // 永久会员没有过期时间
+    obj.expired = 0; // 永久会员状态未过期
+    obj.remainTime = 99999;
+    obj.isPaying = 1;
+}
+
+if (url.includes('/weekly/exchange')) {
+     if (obj.memberCardExchange) {
+    obj.memberCardExchange.forEach(card => {
+    card.canExchange = 1; 
+    card.days = 9999;  
+    card.price = 0;
+    card.needFreeCardCnt = 1;
+       });
+   }
+    obj.isMCardVip = 1;  // 设置为会员
+    obj.infiniteCard.day = 9999;
+    obj.infiniteCard.paying = 1;
+}
+
+if (url.includes('/storyfeed/getRecentBooks')) {
+   if(obj.items) {
+   obj.items.forEach(function(item) {
+    item.book.free = 1; // 设置书籍为免费
+    item.book.originalPrice = 0; // 设置原价为 0
+    item.book.centPrice = 0;
+    item.book.payType = 0;    
+    });
+    }
+}
+
+if (url.includes('pay/balance')) {
+   obj.giftBalance = 9999;
+   obj.peerBalance = 999999;
+   obj.paperBalance = 999999;
+   obj.incentive = 9999;
+   obj.balance = 99999;
+}
+
+if (url.includes('user/profile')) {
+   obj.role = 1;
+}
+
+$done({ body: JSON.stringify(obj) });
