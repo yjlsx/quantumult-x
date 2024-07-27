ff37962ec88dde25f430ff76a376a712af891757
yjlsx <2413704955@qq.com>
2024-07-27 15:17:17 +0800

    .

--- ceshi/111/wxds.js
+++ ceshi/111/wxds.js
@@ -18,6 +18,7 @@
 ^https://i.\weread\.qq\.com/pay/balance url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
 ^https://i\.weread\.qq\.com/pay/membercardexitems url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
 ^https://i\.weread\.qq\.com/pay/membercardfrozen url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
+^https://i\.weread\.qq\.com/book/getProgress url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/wxds.js
 ^https://i\.weread\.qq\.com/updateConfig url reject
 [mitm] 
 hostname = i.weread.qq.com
@@ -114,6 +115,10 @@ if (url.includes('/pay/membercardfrozen')) {
     obj.errcode = 0;
     obj.errlog = "";
     obj.errmsg = "兑换成功";
+}
+
+if (url.includes('/book/getProgress')) {
+    obj.canFreeRead = 1;
 }
 
 
