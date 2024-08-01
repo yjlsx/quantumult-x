469c527004d1a89f28b35e4db9aa178f4df96e44
yjlsx <2413704955@qq.com>
2024-08-01 17:50:29 +0800

    .

--- ceshi/111/quark1.js
+++ ceshi/111/quark1.js
@@ -4,10 +4,11 @@
 [rewrite_local]
 https?:\/\/drive.*\.quark\.cn\/.+\/clouddrive\/(member.+|distribute\/detail.+|capacity\/growth\/info) url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/quark1.js
 ^https:\/\/coral2\.quark\.cn\/quark\/v2\/(queryMemberInfo|getMemberMessage|home) url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/quark1.js
+^https:\/\/order-api\.sm\.cn\/api\/(payorder\/v1\/precreate|member\/v1\/lotteryDraw|member\/v1\/center) url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/quark1.js
 
 
 [mitm]
-hostname = drive*.quark.cn, coral2.quark.cn
+hostname = drive*.quark.cn, coral2.quark.cn, order-api.sm.cn
 
 *************************************/
 
@@ -21,7 +22,7 @@ const vipc = '/clouddrive/capacity/growth/info';
 
 if ($request.url.indexOf(vipa) != -1){
   yjlsx.data = {
-    "member_type": "SUPER_VIP",
+    "member_type": "Z_VIP",
     "image_backup": 1,
     "deep_recycle_stat": {
       "recycle_normal_serve_days": 10,
@@ -47,10 +48,10 @@ if ($request.url.indexOf(vipa) != -1){
     "extend_capacity_composition": {},
     "is_new_user": false,
     "member_status": {
-      "Z_VIP": "UNPAID",
-      "VIP": "UNPAID",
+      "Z_VIP": "PAID",
+      "VIP": "PAID",
       "SUPER_VIP": "PAID",
-      "MINI_VIP": "UNPAID"
+      "MINI_VIP": "PAID"
     },
     "secret_total_capacity": 109951162777600,
     "subscribe_pay_channel_map": {},
@@ -66,10 +67,10 @@ if ($request.url.indexOf(vipb) != -1){
     "last_id": 0,
     "last_page": true,
     "distribute_detail": [{
-      "title": "SVIP会员",
+      "title": "SVIP+会员",
       "product_category": "MEMBER",
       "member_detail": {
-        "member_type": "SUPER_VIP",
+        "member_type": "Z_VIP",
         "capacity": 109951162777600
       },
       "expired_at": 4092599349000
@@ -79,7 +80,7 @@ if ($request.url.indexOf(vipb) != -1){
 
 if ($request.url.indexOf(vipc) != -1){
   yjlsx.data = {
-    "member_type": "SUPER_VIP",
+    "member_type": "Z_VIP",
     "super_vip_exp_at": 4092599349000,
     "use_capacity": 55029395707,
     "cap_growth": {
@@ -134,7 +135,7 @@ if ($request.url.indexOf('/quark/v2/queryMemberInfo') !== -1) {
   // 修改 memberList 的 memberType
   if (yjlsx && yjlsx.data && Array.isArray(yjlsx.data.memberList)) {
     yjlsx.data.memberList.forEach(item => {
-      item.memberType = "SUPER_VIP";
+      item.memberType = "Z_VIP";
     });
   }
 }
@@ -151,16 +152,63 @@ if ($request.url.indexOf('/quark/v2/home') !== -1) {
   if (yjlsx && yjlsx.data && Array.isArray(yjlsx.data.memberInfoList)) {
     yjlsx.data.memberInfoList.forEach(member => {
       if (member.name === '网盘') {
-        member.memberType = 'SUPER_VIP';
+        member.memberType = 'Z_VIP';
         member.expireTime = 4092599349000;
         member.productInfo.memberStatus = "PAID";
+        member.productInfo.nameplateDesc = "永久SVIP";
+        member.diffDay = -1;
+        member.lowAmount = 0;
+        member.icon88 = "https://img.alicdn.com/imgextra/i4/O1CN01KklBdX1ILiABnDPVS_!!6000000000877-2-tps-108-108.png";
+      }
+      if (member.name === '扫描王') {
+        member.memberType = 'photo_vip';
+        member.expireTime = 4092599349000;
+        member.productInfo.memberStatus = "PAID";
+        member.productInfo.nameplateDesc = "永久扫描王";
         member.diffDay = -1;
         member.lowAmount = 0;
       }
+      if (member.name === '文档') {
+        member.memberType = 'photo_vip';
+        member.expireTime = 4092599349000;
+        member.productInfo.memberStatus = "PAID";
+        member.productInfo.nameplateDesc = "永久SVIP";
+        member.diffDay = -1;
+        member.lowAmount = 0;
+      }
+
     });
   }
 }
 
+
+if ($request.url.indexOf('/api/payorder/v1/precreate') !== -1) {
+  // 修改 hasVip 为 true
+  if (yjlsx && yjlsx.data) {
+    yjlsx.code = 0;
+    yjlsx.status = 200;
+    yjlsx.data.created = true;
+    yjlsx.data.free = true;
+  }
+}
+
+if ($request.url.indexOf('member\/v1\/lotteryDraw') !== -1) {
+  if (yjlsx && yjlsx.data) {
+    yjlsx.code = 0;
+    yjlsx.status = 200;
+  }
+}
+
+if ($request.url.indexOf('member\/v1\/center') !== -1) {
+  if (yjlsx && yjlsx.data) {
+    yjlsx.code = 0;
+    yjlsx.status = 200;
+    yjlsx.data.userInfo.is88Freeze = true;    
+    yjlsx.data.userInfo.memberType = 'Z_VIP';
+    yjlsx.data.userInfo.memberStatus = "PAID";
+    yjlsx.data.userInfo.vipExpirationTime = 4092599349000;
+  }
+}
 
 
 $done({body : JSON.stringify(yjlsx)});
