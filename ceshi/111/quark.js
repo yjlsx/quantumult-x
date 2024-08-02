4051b3af219f8d0a5087d00feb60207556f7ccb6
yjlsx <2413704955@qq.com>
2024-08-02 22:04:15 +0800

    .

--- ceshi/111/quark.js
+++ ceshi/111/quark.js
@@ -314,15 +314,15 @@ if ($request.url.indexOf('/clouddrive/file/v2/play') !== -1) {
     if (video.video.info) {
       video.member_right = "normal"; 
       video.right = "normal"; 
-      video.resolution = "low"; 
+      video.resolution = "high"; 
         } else {
       video.member_right = "svip"; 
       video.right = "svip"; 
       video.resolution = "high"; 
          }
      });
-yjlsx.data.default_resolution = "high"; 
-yjlsx.data.origin_default_resolution = "high"; 
+    yjlsx.data.default_resolution = "high"; 
+    yjlsx.data.origin_default_resolution = "high"; 
   }
 }
 
