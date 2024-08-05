0b911efc643d94b74293acadbff81cbaf13bd9ef
yjlsx <2413704955@qq.com>
2024-08-05 12:07:18 +0800

    .

--- ceshi/111/mgtvjf.js
+++ ceshi/111/mgtvjf.js
@@ -4,70 +4,74 @@
 ^https:\/\/as\.mgtv\.com\/client\/user\/user_vip_coin$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/mgtvjf.js
 
 # 处理第二个 URL 响应
-^https:\/\/as\.mgtv\.com\/client\/user\/user_info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/mgtvjf.js
-^https:\/\/vipact3\.api\.mgtv\.com\/api\/v1\/act\/assets\/idxnum url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/mgtvjf.js
+^https:\/\/as\.mgtv\.com\/client\/user\/user_info$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/mgtvjf.js
+^https:\/\/vipact3\.api\.mgtv\.com\/api\/v1\/act\/assets\/idxnum$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/mgtvjf.js
+#^https:\/\/nuc\.api\.mgtv\.com\/GetUserInfo$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/mgtvjf.js
+
+
 [mitm]
 hostname = as.mgtv.com, vipact3.api.mgtv.com
 *************************************/
-// 处理第一个 URL 响应
-if ($request.url.indexOf('/client/user/user_vip_coin') !== -1) {
-  const body = $response.body;
-  const obj = JSON.parse(body);
 
+let body = $response.body;
+let obj = JSON.parse(body);
+
+if ($request.url.indexOf('/client/user/user_vip_coin') !== -1) {
   if (obj.data) {
     obj.data.point = 99999;
     obj.data.stat = 99999;
   }
-
-  $done({body: JSON.stringify(obj)});
-}
+} 
 
 if ($request.url.indexOf('/api/v1/act/assets/idxnum') !== -1) {
-  const body = $response.body;
-  const obj = JSON.parse(body);
-
   if (obj.data) {
     obj.data.idx.vcoin = 99999;
     obj.data.idx.union_vip = 10;
   }
+} 
 
-  $done({body: JSON.stringify(obj)});
-}
-
-// 处理第二个 URL 响应
 if ($request.url.indexOf('/client/user/user_info') !== -1) {
-  const body = $response.body;
-  const json = JSON.parse(body);
-
-  if (json.data) {
+  if (obj.data) {
     // 更新所有结束日期字段
-    json.data.vip_end_time_pc = "2099-09-09";
-    json.data.mpp_svip_end_date = "2099-09-09";
-    json.data.bigscreen_vip_end_date = "2099-09-09";
-    json.data.vip_end_date = "2099-09-09";
-    json.data.contract_full_screen_vip_end_date = "2099-09-09";
-    json.data.universal_pc_mobile_vip_end_date = "2099-09-09";
-    json.data.music_vip_end_time = "2099-09-09";
-    json.data.vip_end_time_svip = "2099-09-09";
-    json.data.contract_pc_mobile_vip_end_date = "2099-09-09";
-    json.data.universal_full_screen_vip_end_date = "2099-09-09";
-    json.data.vip_end_time_fs = "2099-09-09";
+    obj.data.vip_end_time_pc = "2099-09-09";
+    obj.data.mpp_svip_end_date = "2099-09-09";
+    obj.data.bigscreen_vip_end_date = "2099-09-09";
+    obj.data.vip_end_date = "2099-09-09";
+    obj.data.contract_full_screen_vip_end_date = "2099-09-09";
+    obj.data.universal_pc_mobile_vip_end_date = "2099-09-09";
+    obj.data.music_vip_end_time = "2099-09-09";
+    obj.data.vip_end_time_svip = "2099-09-09";
+    obj.data.contract_pc_mobile_vip_end_date = "2099-09-09";
+    obj.data.universal_full_screen_vip_end_date = "2099-09-09";
+    obj.data.vip_end_time_fs = "2099-09-09";
 
     // 更新VIP相关字段
-    json.data.is_mpp_svip = 1;
-    json.data.music_vip = 1;
-    json.data.vip_id = 1;
-    json.data.vip_name = "svip";
+    obj.data.is_mpp_svip = 1;
+    obj.data.music_vip = 1;
+    obj.data.vip_id = 1;
+    obj.data.vip_name = "svip";
 
     // 更新成长数据
-    if (json.data.growth) {
-      json.data.growth.level = 9;
-      json.data.growth.score = 99999;
+    if (obj.data.growth) {
+      obj.data.growth.level = 9;
+      obj.data.growth.score = 99999;
     }
 
     // 更新剩余天数
-    json.data.vip_end_days = 99999;
+    obj.data.vip_end_days = 99999;
+  }
+}
+
+
+// 检查是否是正确的 URL
+if ($request.url.indexOf('/GetUserInfo') !== -1) {
+  // 修改响应体为 VIP，过期时间2099年12月31，积分99999，等级9
+  if (obj.data) {
+    obj.data.isVip = 1; // 设置为 VIP
+    obj.data.vipExpiretime = 4102444800; // 设置过期时间为 2099-12-31
+    obj.data.vipinfo.growth.score = 99999; // 设置积分
+    obj.data.vipinfo.growth.level = 9; // 设置等级
   }
 
-  $done({body: JSON.stringify(json)});
-}
+
+$done({body: JSON.stringify(obj)});
