fc7b67b954ddd8c4fd3898333aac814fe91d6b31
yjlsx <2413704955@qq.com>
2024-08-10 09:35:54 +0800

    .

--- rewrite/MGTV/mgtv3.js
+++ rewrite/MGTV/mgtv3.js
@@ -2,7 +2,7 @@
 [rewrite local]
 ^https:\/\/vipact3\.api\.mgtv\.com\/api\/v1\/act\/assets\/idxnum url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/rewrite/MGTV/mgtv3.js
 ^http:\/\/vipact3\.api\.mgtv\.com\/api\/v1\/app\/vip\/center\/user\/info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/rewrite/MGTV/mgtv3.js
-^https:\/\/vipact3\.api\.mgtv\.com\/api\/v1\/app\/vip\/center\/user\/info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/rewrite/MGTV/mgtv3.js
+^https:\/\/vipact3\.api\.mgtv\.com\/api\/v1\/app\/vip\/center\/vip\/info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/rewrite/MGTV/mgtv3.js
 
 *
 [mitm]
@@ -48,8 +48,15 @@ function modifyResponse(response) {
         obj.data.score = 99999; // 修改为适当的积分
         obj.data.user_type = 2; // 修改为 VIP 用户类型
         }
+}
+
+
+ if ($request.url.indexOf('/api/v1/app/vip/center/vip/info') !== -1) {
         if (obj.data.userinfo) {
+        obj.data.vip_end_time_desc = "VIP 特权有效至 2099-12-31";
+        obj.data.vip_end_time = 4102444800; // 2099-12-31 的时间戳
         obj.data.userinfo.music_vip_end_time = "2099-09-09 00:00:00";
+        obj.data.userinfo.universal_pc_mobile_vip_end_date = "2099-09-09";
         obj.data.userinfo.music_vip = 1;
         obj.data.userinfo.vip_end_time_pc = "2099-09-09 00:00:00";
         obj.data.userinfo.vip_name = "SVIP";
