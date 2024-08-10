a877673d8f95128863f5b991769d7a4666f06c40
yjlsx <2413704955@qq.com>
2024-08-10 09:54:45 +0800

    .

--- rewrite/MGTV/mgtv3.js
+++ rewrite/MGTV/mgtv3.js
@@ -55,18 +55,25 @@ function modifyResponse(response) {
         if (obj.data.userinfo) {
         obj.data.vip_end_time_desc = "VIP 特权有效至 2099-12-31";
         obj.data.vip_end_time = 4102444800; // 2099-12-31 的时间戳
-        obj.data.userinfo.music_vip_end_time = "2099-09-09 00:00:00";
-        obj.data.userinfo.universal_pc_mobile_vip_end_date = "2099-09-09";
+        obj.data.userinfo.music_vip_end_time = "2099-12-31 00:00:00";
+        obj.data.userinfo.universal_pc_mobile_vip_end_date = "2099-12-31";
+        obj.data.userinfo.contract_pc_mobile_vip_end_date = "2099-12-31";
+        obj.data.userinfo.universal_full_screen_vip_end_date = "2099-12-31";
+        obj.data.userinfo.contract_pc_screen_vip_end_date = "2099-12-31";
+        obj.data.userinfo.bigscreen_vip_end_date = "2099-12-31";
+        obj.data.userinfo.bigscreen_vip_available = 1;
+        obj.data.userinfo.contract_full_screen_vip_flag = 1;
         obj.data.userinfo.music_vip = 1;
-        obj.data.userinfo.vip_end_time_pc = "2099-09-09 00:00:00";
+        obj.data.userinfo.vip_end_time_pc = "2099-12-31 00:00:00";
         obj.data.userinfo.vip_name = "SVIP";
-        obj.data.userinfo.vip_end_date = "2099-09-09";
+        obj.data.userinfo.vip_end_date = "2099-12-31";
         obj.data.userinfo.growth.score = 99999;
         obj.data.userinfo.growth.level = 9;
         obj.data.userinfo.is_mpp_svip = 1;
-        obj.data.userinfo.vip_end_time_fs = "2099-09-09 00:00:00";
+        obj.data.userinfo.mpp_svip_end_date = "2099-12-31";
+        obj.data.userinfo.vip_end_time_fs = "2099-12-31 00:00:00";
         obj.data.userinfo.vip_end_days = 9999;
-        obj.data.userinfo.vip_end_time_svip = "2099-09-09 00:00:00";
+        obj.data.userinfo.vip_end_time_svip = "2099-12-31 00:00:00";
         obj.data.userinfo.vip_id = "mpp_svip";
     }
  }
