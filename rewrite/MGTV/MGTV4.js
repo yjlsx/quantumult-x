686d14d2458f11163850cfa8975ad2b826854a47
yjlsx <2413704955@qq.com>
2024-08-12 09:31:35 +0800

    .

--- rewrite/MGTV/MGTV4.js
+++ rewrite/MGTV/MGTV4.js
@@ -5,29 +5,47 @@ let headers = $request.headers || {};
 // 检查请求的 URL 是否为目标地址
 if ($request.url.indexOf('https://mobile-stream.api.mgtv.com/v1/video/source') !== -1) {
 
-    // 第一步：删除不需要的 query 参数
-    delete query['pvod'];
-    delete query['drmcp'];
-    delete query['sdrm'];
-    delete query['ignorePreviewAndOthers'];
-    delete query['exdef']; // 移除 exdef 参数
+    // 定义需要保留的查询参数和默认值
+    const requiredParams = {
+        'seekSourceType': '0',
+        'keepPlay': '0',
+        'enableDolphinEncrypted': '0',
+        'maxSpeed': '3',
+        'fileSourceType': '1',
+        'disableCelluar': '0',
+        'disableP2P': '0',
+        'disableKeyframe': '0',
+        'enableDolphinAntiLeech': '0',
+        'enableDolphinSDK': '0',
+        'localAbr': '0',
+        'toDataType': '201',
+        'toModuleId': '14',
+        'mediaType': '0',
+        'plId': '0'
+        // 添加任何其他默认值或固定值参数
+    };
 
-    // 第二步：添加或修改 query 参数
-    query['seekSourceType'] = '0';
-    query['keepPlay'] = '0';
-    query['enableDolphinEncrypted'] = '0';
-    query['maxSpeed'] = '3';
-    query['fileSourceType'] = '1';
-    query['disableCelluar'] = '0';
-    query['disableP2P'] = '0';
-    query['disableKeyframe'] = '0';
-    query['enableDolphinAntiLeech'] = '0';
-    query['enableDolphinSDK'] = '0';
-    query['localAbr'] = '0';
-    query['toDataType'] = '201';
-    query['toModuleId'] = '14';
-    query['mediaType'] = '0';
-    query['plId'] = '0';
+    // 定义排序顺序
+    const paramOrder = [
+        'seekSourceType', 'keepPlay', 'enableDolphinEncrypted', 'maxSpeed', 'fileSourceType', 
+        'disableCelluar', 'disableP2P', 'disableKeyframe', 'enableDolphinAntiLeech', 'enableDolphinSDK', 
+        'localAbr', 'toDataType', 'toModuleId', 'mediaType', 'plId'
+        // 添加需要的其他参数，如果有的话
+    ];
+
+    // 删除不需要的查询参数
+    for (let param in query) {
+        if (!(param in requiredParams)) {
+            delete query[param];
+        }
+    }
+
+    // 添加或更新查询参数
+    paramOrder.forEach(param => {
+        if (!(param in query)) {
+            query[param] = requiredParams[param];
+        }
+    });
 
     // 处理 Cookie
     let cookies = headers['Cookie'] || '';
@@ -36,7 +54,7 @@ if ($request.url.indexOf('https://mobile-stream.api.mgtv.com/v1/video/source') !
     headers['Cookie'] = newCookie;
 
     // 构建新的 URL
-    const newUrl = $request.url.split('?')[0] + '?' + Object.keys(query).map(key => key + '=' + encodeURIComponent(query[key])).join('&');
+    const newUrl = $request.url.split('?')[0] + '?' + paramOrder.map(key => `${key}=${encodeURIComponent(query[key] || '')}`).join('&');
 
     // 返回新的重写请求
     $done({url: newUrl, headers: headers});
