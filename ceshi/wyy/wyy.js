8c0f6fc496db6f7790d10ce7ea5623e341dae4c1
yjlsx <2413704955@qq.com>
2024-08-15 01:53:12 +0800

    .

--- ceshi/wyy/wyy.js
+++ ceshi/wyy/wyy.js
@@ -47,9 +47,12 @@
 ^https?:\/\/d[1234]\.music\.126\.net/.*\.gr url reject
 #会员中心
 ^https:\/\/interface\.music\.163\.com\/api\/vip-center-bff\/float\/data url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/wyy/wyy.js
+^https:\/\/interface\.music\.163\.com\/weapi\/vip-center-bff\/float\/data url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/wyy/wyy.js
 ^https:\/\/interface3\.music\.163\.com\/api\/music-vip-membership\/cashier\/info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/wyy/wyy.js
 ^https:\/\/interface\.music\.163\.com\//weapi\/music-vip-membership\/cashier\/info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/wyy/wyy.js
 
+^https:\/\/music\.163\.com\/weapi\/batch\?csrf_token$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/wyy/wyy.js
+ 
 
 
 
@@ -63,7 +66,7 @@ hostname = *.music.163.com, interface*.music.163.com,  *.music.126.net
 let body = $response.body;
 let obj = JSON.parse(body);
 
-if ($request.url.indexOf('/api/vip-center-bff/float/data') !== -1) {
+if ($request.url.indexOf('/vip-center-bff/float/data') !== -1) {
    if (obj.data && obj.data.vipInfo) {
     obj.data.vipInfo.redVipAnnualCount = 1;
     obj.data.vipInfo.redVipLevel = 7;
@@ -76,8 +79,8 @@ if ($request.url.indexOf('/api/vip-center-bff/float/data') !== -1) {
     obj.data.vipInfo.associator.vipLevel = 7;
     obj.data.vipInfo.associator.expireTime = 4102358400000;
     obj.data.vipInfo.associator.iconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32582186486/9f31/5cfe/207c/2846c11ce0bd05aae1754aed7e63ca58.png"; //vip1静态
-    obj.data.vipInfo.redplus.dynamicIconUrl = null  //svip1动态
-    obj.data.vipInfo.redplus.iconUrl = null; //svip1静态
+    obj.data.vipInfo.redplus.dynamicIconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32815146704/bbb8/496f/6cdb/930f24fcdf7276ef00b2de12f71325d7.png";  //svip1动态
+    obj.data.vipInfo.redplus.iconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32815146704/bbb8/496f/6cdb/930f24fcdf7276ef00b2de12f71325d7.png"; //svip1静态
     obj.data.vipInfo.redplus.vipCode = 5;
     obj.data.vipInfo.redplus.expireTime = 4102358400000;
     obj.data.vipInfo.redplus.vipLevel = 7;
@@ -104,7 +107,7 @@ if ($request.url.indexOf('/music-vip-membership/cashier/info') !== -1) {
     obj.data.vip.associator.vipLevel = 7;
     obj.data.vip.associator.expireTime = 4102358400000;
     obj.data.vip.associator.iconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32582186486/9f31/5cfe/207c/2846c11ce0bd05aae1754aed7e63ca58.png"; //vip1静态
-    obj.data.vip.redplus.dynamicIconUrl = "https://p5.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32582306080/a905/24c9/63cd/1d065fb7c32a5fe002d223f9ed8945f3.png";  //svip1动态
+    obj.data.vip.redplus.dynamicIconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32815146704/bbb8/496f/6cdb/930f24fcdf7276ef00b2de12f71325d7.png";  //svip1动态
     obj.data.vip.redplus.iconUrl = "https://p5.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32582306080/a905/24c9/63cd/1d065fb7c32a5fe002d223f9ed8945f3.png"; //svip1静态
     obj.data.vip.redplus.vipCode = 0;
     obj.data.vip.redplus.expireTime = 4102358400000;
@@ -112,9 +115,37 @@ if ($request.url.indexOf('/music-vip-membership/cashier/info') !== -1) {
     obj.data.vip.userVipStatus = [ 10, 15, 25 ];
     obj.data.user.account.vipType = 15;
     obj.data.user.account.status = 1;
-    obj.data.user.profile.vipType = 1;
+    obj.data.user.profile.vipType = 6;  //11是vip
     obj.data.user.profile.accountType = 15;
     }
+}
+
+if ($request.url.indexOf('/weapi/batch?csrf_token') !== -1) {
+    obj["/api/nuser/account/get"].account.vipType = 11;
+    obj["/api/nuser/account/get"].profile.vipType = 11;
+    obj["/api/nuser/account/get"].profile.accountType = 1;
+    obj["/api/music-vip-membership/front/vip/info"].data.redVipLevel = 7;
+    obj["/api/music-vip-membership/front/vip/info"].data.musicPackage.vipCode = 220;
+    obj["/api/music-vip-membership/front/vip/info"].data.musicPackage.iconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32582186486/9f31/5cfe/207c/2846c11ce0bd05aae1754aed7e63ca58.png"; //vip1静态
+    obj["/api/music-vip-membership/front/vip/info"].data.musicPackage.vipLevel = 7;
+    obj["/api/music-vip-membership/front/vip/info"].data.musicPackage.expireTime = 4102358400000;
+    obj["/api/music-vip-membership/front/vip/info"].data.associator.dynamicIconUrl = "https://p5.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32141292744/0634/5100/c09f/eeca8bd06770efbff522a3b77627e2d4.png"; //vip1动态
+    obj["/api/music-vip-membership/front/vip/info"].data.associator.vipCode = 100;
+    obj["/api/music-vip-membership/front/vip/info"].data.associator.vipLevel = 7;
+    obj["/api/music-vip-membership/front/vip/info"].data.associator.expireTime = 4102358400000;
+    obj["/api/music-vip-membership/front/vip/info"].data.associator.iconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32582186486/9f31/5cfe/207c/2846c11ce0bd05aae1754aed7e63ca58.png"; //vip1静态
+    obj["/api/music-vip-membership/front/vip/info"].data.redplus.dynamicIconUrl = "https://p6.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32815146704/bbb8/496f/6cdb/930f24fcdf7276ef00b2de12f71325d7.png";  //svip1动态
+    obj["/api/music-vip-membership/front/vip/info"].data.redplus.iconUrl = "https://p5.music.126.net/obj/wonDlsKUwrLClGjCm8Kx/32582306080/a905/24c9/63cd/1d065fb7c32a5fe002d223f9ed8945f3.png"; //svip1静态
+    obj["/api/music-vip-membership/front/vip/info"].data.redplus.vipCode = 0;
+    obj["/api/music-vip-membership/front/vip/info"].data.redplus.expireTime = 4102358400000;
+    obj["/api/music-vip-membership/front/vip/info"].data.redplus.vipLevel = 7;
+    obj["/api/purchased/redvip/vipstatus"].userVipStatus = [ 10, 15, 25 ];
+
+   obj["/api/pendant/frontend/list"].data.forEach(item => {
+    item.pendantList.forEach(pendant => {
+        pendant.hasUsePermission = true;
+       });
+   });
 }
 
 
