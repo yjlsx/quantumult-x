d40bce0cf860ec5b80f0a3a5549a117e1c4f5a3a
yjlsx <2413704955@qq.com>
2024-08-19 01:56:15 +0800

    .

--- ceshi/111/migutv.js
+++ ceshi/111/migutv.js
@@ -22,109 +22,20 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 if ($request.url.indexOf('/ability/v2/member-rights/miguvideo') !== -1) {
-  obj.body.icons.memberInfos.province_biz_gansu.rights.isTiyutong = true;
-  obj.body.icons.memberInfos.province_biz_gansu.rights.isDiamond = true;
-  obj.body.icons.memberInfos.province_biz_gansu.rights.isNba = true;
-  obj.body.icons.memberInfos.province_biz_gansu.rights.isZuqiuvip = true;
-  obj.body.icons.memberInfos.province_biz_gansu.rights.skipBeforeAd = true;
-  obj.body.icons.memberInfos.province_biz_gansu.rights.isUfc = true;
-  obj.body.icons.memberInfos.province_biz_gansu.rights.isVip = true;
-  obj.body.icons.memberInfos.province_biz_gansu.rights.isCba = true;
-  obj.body.icons.memberInfos.province_biz_gansu.memberGroup = 4;
-  obj.body.icons.memberInfos.province_biz_gansu.title = "钻石会员";
-  obj.body.icons.memberInfos.province_biz_gansu.containMember = true;
-  obj.body.icons.memberInfos.province_biz_gansu.icons = "diamond";
-      if(obj.body.icons.memberInfos.ufc.rights) {
-     obj.body.icons.memberInfos.ufc.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.ufc.rights.isDiamond = true;
-     obj.body.icons.memberInfos.ufc.rights.isNba = true;
-     obj.body.icons.memberInfos.ufc.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.ufc.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.ufc.rights.isUfc = true;
-     obj.body.icons.memberInfos.ufc.rights.isVip = true;
-     obj.body.icons.memberInfos.ufc.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.migu_dazhanbao.rights) {
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.isDiamond = true;
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.isNba = true;
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.isUfc = true;
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.isVip = true;
-     obj.body.icons.memberInfos.migu_dazhanbao.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.zuqiuvip.rights) {
-     obj.body.icons.memberInfos.zuqiuvip.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.zuqiuvip.rights.isDiamond = true;
-     obj.body.icons.memberInfos.zuqiuvip.rights.isNba = true;
-     obj.body.icons.memberInfos.zuqiuvip.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.zuqiuvip.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.zuqiuvip.rights.isUfc = true;
-     obj.body.icons.memberInfos.zuqiuvip.rights.isVip = true;
-     obj.body.icons.memberInfos.zuqiuvip.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.nba.rights) {
-     obj.body.icons.memberInfos.nba.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.nba.rights.isDiamond = true;
-     obj.body.icons.memberInfos.nba.rights.isNba = true;
-     obj.body.icons.memberInfos.nba.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.nba.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.nba.rights.isUfc = true;
-     obj.body.icons.memberInfos.nba.rights.isVip = true;
-     obj.body.icons.memberInfos.nba.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.tiyutong.rights) {
-     obj.body.icons.memberInfos.tiyutong.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.tiyutong.rights.isDiamond = true;
-     obj.body.icons.memberInfos.tiyutong.rights.isNba = true;
-     obj.body.icons.memberInfos.tiyutong.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.tiyutong.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.tiyutong.rights.isUfc = true;
-     obj.body.icons.memberInfos.tiyutong.rights.isVip = true;
-     obj.body.icons.memberInfos.tiyutong.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.cba.rights) {
-     obj.body.icons.memberInfos.cba.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.cba.rights.isDiamond = true;
-     obj.body.icons.memberInfos.cba.rights.isNba = true;
-     obj.body.icons.memberInfos.cba.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.cba.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.cba.rights.isUfc = true;
-     obj.body.icons.memberInfos.cba.rights.isVip = true;
-     obj.body.icons.memberInfos.cba.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.black_diamond.rights) {
-     obj.body.icons.memberInfos.black_diamond.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.black_diamond.rights.isDiamond = true;
-     obj.body.icons.memberInfos.black_diamond.rights.isNba = true;
-     obj.body.icons.memberInfos.black_diamond.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.black_diamond.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.black_diamond.rights.isUfc = true;
-     obj.body.icons.memberInfos.black_diamond.rights.isVip = true;
-     obj.body.icons.memberInfos.black_diamond.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.diamond.rights) {
-     obj.body.icons.memberInfos.diamond.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.diamond.rights.isDiamond = true;
-     obj.body.icons.memberInfos.diamond.rights.isNba = true;
-     obj.body.icons.memberInfos.diamond.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.diamond.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.diamond.rights.isUfc = true;
-     obj.body.icons.memberInfos.diamond.rights.isVip = true;
-     obj.body.icons.memberInfos.diamond.rights.isCba = true;
-       }
-      if(obj.body.icons.memberInfos.province_biz_guangzhou.rights) {
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.isTiyutong = true;
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.isDiamond = true;
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.isNba = true;
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.isZuqiuvip = true;
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.skipBeforeAd = true;
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.isUfc = true;
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.isVip = true;
-     obj.body.icons.memberInfos.province_biz_guangzhou.rights.isCba = true;
-       }
-
+    // 遍历每个会员信息
+   for (let key in body.body.memberInfos) {
+    let rights = body.body.memberInfos[key].rights;
+    
+    // 将所有目标字段设置为true
+    rights.isTiyutong = true;
+    rights.isNba = true;
+    rights.isDiamond = true;
+    rights.isZuqiuvip = true;
+    rights.skipBeforeAd = true;
+    rights.isUfc = true;
+    rights.isVip = true;
+    rights.isCba = true;
+        }
   }
 
 if ($request.url.indexOf('/playurl/v1/play/playurl') !== -1) {
