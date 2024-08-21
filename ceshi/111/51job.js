b3b537b7a005c52d9812911a65088dffb8f30568
yjlsx <2413704955@qq.com>
2024-08-21 11:44:38 +0800

    .

--- ceshi/111/51job.js
+++ ceshi/111/51job.js
@@ -23,75 +23,101 @@ let obj = JSON.parse(body);
 // 获取请求 URL
 let url = $request.url;
 
-// 根据不同的 URL 修改响应体内容
 try {
+    // 修改 /open/my-page/v2 的响应体
     if (url.includes('/open/my-page/v2')) {
         if (obj.resultbody) {
             obj.resultbody.vipInfo.isVip = true;
             obj.resultbody.vipInfo.effectiveDate = "2099-12-31T23:59:59Z";
             obj.resultbody.showManagementPage = 2;
         }
-    } else if (url.includes('/open/vip')) {
-        if (obj.resultbody.hasCompetitivenessService) {
-            obj.resultbody.hasCompetitivenessService = true;
-            obj.resultbody.remainCompetitivenessCount = true;
-            obj.resultbody.isVip = true;
-         }
-        if (obj.resultbody.isVip) {
+    }
+
+    // 修改 /open/vip 的响应体
+    if (url.includes('/open/vip')) {
+        if (obj.resultbody.effectiveDate) {
             obj.resultbody.isVip = true;
             obj.resultbody.effectiveDate = "2099-12-31T23:59:59Z";
+
+            if (obj.resultbody.hasCompetitivenessService) {
+                obj.resultbody.hasCompetitivenessService = true;
+                obj.resultbody.remainCompetitivenessCount = true;
+            }
+
+            if (obj.resultbody.interestedInVO) {
+                obj.resultbody.interestedInVO.maxViewedCount = 99999;
+            }
+
+            if (obj.resultbody.vipInfoVO) {
+                obj.resultbody.vipInfoVO.isVip = true;
+                obj.resultbody.vipInfoVO.effectiveDate = "2099-12-31T23:59:59Z";
+            }
+
+            if (obj.resultbody.competitivenessVO) {
+                obj.resultbody.competitivenessVO.maxViewedCount = 99999;
+                obj.resultbody.competitivenessVO.isCompetitivenessAnalysis = true;
+            }
+
+            if (obj.resultbody.resumeRefreshVO) {
+                obj.resultbody.resumeRefreshVO.maxRefreshCount = 99999;
+            }
         }
-        if (obj.resultbody.interestedInVO) {
-            obj.resultbody.interestedInVO.maxViewedCount = 99999;
-        }
-        if (obj.resultbody.vipInfoVO) {
-            obj.resultbody.vipInfoVO.isVip = true;
-            obj.resultbody.vipInfoVO.effectiveDate = "2099-12-31T23:59:59Z";
-        }
-        if (obj.resultbody.competitivenessVO) {
-            obj.resultbody.competitivenessVO.maxViewedCount = 99999;
-            obj.resultbody.competitivenessVO.isCompetitivenessAnalysis = true;
-        }
-        if (obj.resultbody.resumeRefreshVO) {
-            obj.resultbody.resumeRefreshVO.maxRefreshCount = 99999;
-        }
-    }else if (url.includes('/open/equity/equity/duration')) {
+    }
+
+    // 修改 /open/equity/equity/duration 的响应体
+    if (url.includes('/open/equity/equity/duration')) {
         if (obj.resultbody) {
             obj.resultbody.opened = true;
         }
-    }else if (url.includes('/api/payservice/get_ios_service_info.php')) {
+    }
+
+    // 修改 /api/payservice/get_ios_service_info.php 的响应体
+    if (url.includes('/api/payservice/get_ios_service_info.php')) {
         if (obj.hadbuy) {
             obj.hadbuy = "1";
         }
-    }else if (url.includes('/open/job-apply')) {
+    }
+
+    // 修改 /open/job-apply 的响应体
+    if (url.includes('/open/job-apply')) {
         if (obj.resultbody) {
             obj.resultbody.buyService = true;
             obj.resultbody.showOptimize = true;
         }
-    }else if (url.includes('/open/product/monthly-card/rec')) {
-      obj.resultbody.productDetailList.forEach(product => {
-          product.skuList.forEach(sku => {
-             sku.price = 0;
-             sku.originalPrice = 0;
-                });
-           });
-    }else if (url.includes('/open/product/product-list')) {
-    if (obj.resultbody && obj.resultbody.productDetailList) {
-    obj.resultbody.productDetailList.forEach(product => {
-        if (product.skuList) {
-            product.skuList.forEach(sku => {
-                sku.price = 0;
+    }
+
+    // 修改 /open/product/monthly-card/rec 的响应体
+    if (url.includes('/open/product/monthly-card/rec')) {
+        if (obj.resultbody && obj.resultbody.productDetailList) {
+            obj.resultbody.productDetailList.forEach(product => {
+                if (product.skuList) {
+                    product.skuList.forEach(sku => {
+                        sku.price = 0;
+                        sku.originalPrice = 0;
+                    });
+                }
             });
         }
-    });
-    }   
-  }
+    }
+
+    // 修改 /open/product/product-list 的响应体
+    if (url.includes('/open/product/product-list')) {
+        if (obj.resultbody && obj.resultbody.productDetailList) {
+            obj.resultbody.productDetailList.forEach(product => {
+                if (product.skuList) {
+                    product.skuList.forEach(sku => {
+                        sku.price = 0;
+                    });
+                }
+            });
+        }
+    }
+
 } catch (e) {
     // 忽略错误，继续处理其他部分
 }
 
-
-
 body = JSON.stringify(obj);
-$done({body});
+$done({ body });
+
 
