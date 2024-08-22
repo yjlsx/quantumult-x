1f26a279d42d5a52540169df8d1bc20225e67bf1
yjlsx <2413704955@qq.com>
2024-08-22 22:23:24 +0800

    .

--- yjlsx/MGTV/MGTV5.js
+++ yjlsx/MGTV/MGTV5.js
@@ -14,8 +14,8 @@ let urlParams = new URLSearchParams(url.split('?')[1]);
 
 // 更新指定的参数值
 urlParams.set('suuid', 'a88e1d1553248a0337f97e52f4ae7546');
-//urlParams.set('seqId', '05768a1987f60bd0dffba3679cb940e6'); 
-urlParams.set('ticket', '233266506BF03F9FA01EDC6D4C9859AB');
+urlParams.set('seqId', '05768a1987f60bd0dffba3679cb940e6'); 
+//urlParams.set('ticket', '233266506BF03F9FA01EDC6D4C9859AB');
 
 // 重新构建 URL
 let baseUrl = url.split('?')[0];
