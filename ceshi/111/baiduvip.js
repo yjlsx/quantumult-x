3f37019057de7118071b4a33303af841c599d92e
yjlsx <2413704955@qq.com>
2024-08-26 23:52:12 +0800

    .

--- ceshi/111/baiduvip.js
+++ ceshi/111/baiduvip.js
@@ -194,7 +194,7 @@ if ($request.url.indexOf(yike) != -1){
     "product_infos": [{
       "product_id": "12745849497343294855",
       "start_time": 1646534568,
-      "end_time": 4092599349,
+      "end_time": 4102358400,
       "buy_time": 1649994533,
       "tag": "album_vip",
       "order_no": "2203060931530010416"
@@ -202,7 +202,7 @@ if ($request.url.indexOf(yike) != -1){
     "vip_infos": [{
       "tag": "album_vip",
       "start_time": 1646537208,
-      "end_time": 4092599349
+      "end_time": 4102358400
     }],
     "expire_time": 0
   };
@@ -391,17 +391,16 @@ obj.current_product = {
   ];
   obj.previous_product_v2 = {
     "detail_cluster" : "svip",
-    "expired_time" : 0,
+    "expired_time" : 4102358400,
     "cluster" : "vip",
-    "product_type" : "vip2_1y"
+    "product_type" : "vip2_1y_auto"
   };
   obj.previous_product = {
     "detail_cluster" : "svip",
-    "expired_time" : 0,
+    "expired_time" : 4102358400,
     "cluster" : "vip",
-    "product_type" : "vip2_1y"
+    "product_type" : "vip2_1y_auto"
   };
-  obj.netdisk_common_listening_notes_vip.status = 1;
   obj.user_tag =  "{\"is_vip\":1,\"is_svip\":1,\"is_vipv10\":1,\"is_vip_v2\":1,\"is_svip_sign\":0,\"is_vipv2_sign\":0,\"is_scan_vip_sign\":0,\"has_buy_record\":1,\"has_vip_buy_record\":0,\"has_vipv2_buy_record\":0,\"has_svip_buy_record\":0,\"has_buy_vip_svip_record\":0,\"last_buy_record_creat_time\":4102358400,\"is_first_charge\":0,\"is_vip_first_charge\":0,\"notice_user_type\":2,\"notice_user_status\":3,\"last_vip_type\":1,\"last_vip_svip_end_time\":4102358400,\"last_vip_end_time\":4102358400,\"last_vipv2_end_time\":0,\"last_svip_end_time\":4102358400,\"is_first_act\":0,\"activateTime\":1704719871,\"last_v10_end_time\":4102358400,\"last_scan_vip_end_time\":4102358400}";
   obj.user_status = 2;
   obj.user_type = "svip";
