9ac7ff78a9dff3fc3519d6fdb38c9aedeabb6cfb
yjlsx <2413704955@qq.com>
2024-08-28 17:34:25 +0800

    .

---
+++ ceshi/111/zl.js
@@ -0,0 +1,36 @@
+/*
+[rewrite_local]
+# 统一处理脚本
+^https:\/\/cgate\.zhaopin\.com\/mbTrade\/account\/getCommercialMode url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/zl.js
+^https:\/\/cgate\.zhaopin\.com\/mbTrade\/queryAsset\/queryCouponList url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/zl.js
+^https:\/\/cgate\.zhaopin\.com\/mbTrade\/queryAsset\/queryCouponList url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/zl.js
+
+
+
+*
+[mitm]
+hostname = cgate.zhaopin.com
+*/
+
+let body = $response.body;
+let obj = JSON.parse(body);
+
+// 获取请求 URL
+let url = $request.url;
+
+if (url.includes('/mbTrade/account/getCommercialMode')) {
+    obj.data.payUser = true;
+    obj.data.postPay = true;
+    obj.data.rightUser = true;
+}
+if (url.includes('/queryAsset/queryCouponList')) {
+      obj.data.page.total = 99999;
+      obj.data.page.totalPage = 99997;
+}
+
+if (url.includes('/listAggregateCoupon')) {
+    obj.data.couponCount = 99995;
+}
+
+body = JSON.stringify(obj);
+$done({body});
