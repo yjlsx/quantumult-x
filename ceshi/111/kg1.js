7d9e96467ae59dfd5b993782244684f2333e79d3
yjlsx <2413704955@qq.com>
2024-09-16 18:38:54 +0800

    .

--- ceshi/111/kg1.js
+++ ceshi/111/kg1.js
@@ -1,15 +1,31 @@
 /*
 [rewrite_local]
-^https://gateway\.kugou\.com/vipcenter/ios url script-request-header https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kg1.js
+^https://gateway\.kugou\.com/v5/url?album_audio_id url script-request-header https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kg1.js
 
 [mitm]
-hostname = gateway.kugou.com
+hostname = gateway.kugou.com, m.kugou.com
 */
 
+const url = $request.url;
+const headers = $request.headers;
 
-// 请求拦截脚本，去除请求头中的 token
-const modifiedHeaders = $request.headers;
-delete modifiedHeaders['token'];  // 移除 token 头
+if (url.includes("/v5/url?")) {
+    // 解析原请求的 hash 值
+    const hashMatch = url.match(/hash=([0-9a-fA-F]{32})/);
+    const hash = hashMatch ? hashMatch[1] : '';
+    
+    // 修改请求地址
+    const newUrl = `https://m.kugou.com/app/i/getSongInfo.php?cmd=playInfo&hash=${hash}`;
+    
+    // 修改请求头
+    headers['x-router'] = 'm.kugou.com';
+    
+    $done({
+        url: newUrl,
+        headers: headers
+    });
+} else {
+    // 如果不符合条件，则直接返回原响应
+    $done({});
+}
 
-// 返回处理后的请求
-$done({ headers: modifiedHeaders });
