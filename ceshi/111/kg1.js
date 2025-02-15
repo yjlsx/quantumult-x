72d9005f45c50b4a74120c24905826c8a558f214
yjlsx <44745476+yjlsx@users.noreply.github.com>
2025-02-16 01:10:22 +0800

    Update kg1.js

--- ceshi/111/kg1.js
+++ ceshi/111/kg1.js
@@ -1,7 +1,7 @@
 /*
 [rewrite_local]
 ^https://gateway\.kugou\.com/v5/url?album_audio_id url script-request-header https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kg1.js
-^https:\/\/gateway\.kugou\.com\/vipcenter\/ios\?tab=superVip  url script-request-header https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kg1.js
+^https:\/\/gateway\.kugou\.com\/vipcenter\/ios  url script-request-header https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/kg1.js
 [mitm]
 hostname = gateway.kugou.com
 */
@@ -38,19 +38,19 @@ if (url.includes("/vipcenter/ios?is_new_song=0")) {
         'Sec-Fetch-Mode' : `navigate`,
         'Host' : `gateway.kugou.com`,
         'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1`,
-         'Sec-Fetch-Site' : `same-origin`,
+        'Sec-Fetch-Site' : `same-origin`,
         'Referer' : `http://openplat-user.kugou.com/`,
-         'Sec-Fetch-Dest' : `document`,
-        'Accept-Language' : `zh-CN,zh-Hans;q=0.9
-
+        'Sec-Fetch-Dest' : `document`,
+        'Accept-Language' : `zh-CN,zh-Hans;q=0.9`  // 修复了这个问题
     };
 
     // 返回修改后的请求头
     $done({
         headers: newHeaders
     });
-} 
+}
 
+// 最后的默认响应（没有任何特定条件时返回空对象）
 if (true) {
     $done({});
 }
