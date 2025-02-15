1eb1213c4cc84de200309cb9ebc1e83e7601c683
yjlsx <44745476+yjlsx@users.noreply.github.com>
2025-02-15 12:14:10 +0800

    Update kugou2.js

--- ceshi/111/kugou2.js
+++ ceshi/111/kugou2.js
@@ -18,8 +18,7 @@ try {
   if (obj.data && obj.data["{initState}"]) {
     let initState = JSON.parse(obj.data["{initState}"]);
     logStr += "找到 {initState} 节点\n";
-    logStr += "initState 内容： " + JSON.stringify(initState) + "\n";
-
+    
     function modifyVip(vip, path) {
       if (vip) {
         logStr += "修改路径：" + path + "\n";
@@ -28,6 +27,7 @@ try {
         vip.vip_type = 1;
         vip.svip_level = 5;
 
+        // 修改所有时间字段
         const timeFields = ["m_end_time", "vip_end_time", "su_vip_end_time", "svip_y_endtime", "su_vip_y_endtime"];
         timeFields.forEach(field => {
           if (vip[field]) {
@@ -36,6 +36,7 @@ try {
           }
         });
 
+        // 设置 SVIP 权限
         vip.svip99 = 1;
         vip.svip_first_autotype76 = 1;
         vip.svip_first_autotype78 = 1;
@@ -43,42 +44,35 @@ try {
         vip.super_vip_upgrade_month = 12;
         vip.svip_upgrade_info = { days: 9999, activity_id: 1, next_price: 0, autotype: 1, price: 0 };
         vip.su_vip_upgrade_info = { days: 9999, activity_id: 1, next_price: 0, autotype: 1, price: 0 };
-        logStr += "  SVIP字段更新完成\n";
+        logStr += "  SVIP 权限已更新\n";
       } else {
         logStr += `路径 ${path} 数据为空\n`;
       }
     }
 
-    // 调试：打印 props.pageProps.state 的内容
-    if (initState.props?.pageProps?.state) {
-      logStr += "props.pageProps.state 存在\n";
-      console.log("props.pageProps.state:", initState.props.pageProps.state);
-
-      // 检查 funsionData
-      if (initState.props.pageProps.state.funsionData) {
-        logStr += "找到 funsionData\n";
-        if (initState.props.pageProps.state.funsionData.data?.data?.get_vip_info_v3?.data) {
-          modifyVip(initState.props.pageProps.state.funsionData.data.data.get_vip_info_v3.data, 
-                    "props.pageProps.state.funsionData.data.data.get_vip_info_v3.data");
-        } else {
-          logStr += "funsionData 中未找到 VIP 信息\n";
-        }
-      } else {
-        logStr += "未找到 funsionData\n";
-      }
-
-      // 检查 vipInfo
-      if (initState.props.pageProps.state.vipInfo) {
-        modifyVip(initState.props.pageProps.state.vipInfo, "props.pageProps.state.vipInfo");
-      } else {
-        logStr += "未找到 vipInfo\n";
-      }
+    // 修改 funsionData 中的 VIP 信息
+    if (initState.props && 
+        initState.props.pageProps && 
+        initState.props.pageProps.state && 
+        initState.props.pageProps.state.funsionData &&
+        initState.props.pageProps.state.funsionData.data &&
+        initState.props.pageProps.state.funsionData.data.data &&
+        initState.props.pageProps.state.funsionData.data.data.get_vip_info_v3 &&
+        initState.props.pageProps.state.funsionData.data.data.get_vip_info_v3.data
+    ) {
+      modifyVip(
+        initState.props.pageProps.state.funsionData.data.data.get_vip_info_v3.data,
+        "props.pageProps.state.funsionData.data.data.get_vip_info_v3.data"
+      );
     } else {
-      logStr += "未找到 props.pageProps.state\n";
+      logStr += "未找到 funsionData 中的 VIP 信息\n";
     }
 
-    // 检查 initialState.dataVip
-    if (initState.initialState?.dataVip?.data) {
+    // 修改 initialState.dataVip
+    if (initState.initialState && 
+        initState.initialState.dataVip && 
+        initState.initialState.dataVip.data
+    ) {
       modifyVip(initState.initialState.dataVip.data, "initialState.dataVip.data");
     } else {
       logStr += "未找到 initialState.dataVip\n";
@@ -88,11 +82,11 @@ try {
     obj.data["{initState}"] = JSON.stringify(initState);
     logStr += "{initState} 已更新\n";
     
-    $notify("VIP修改成功", "", logStr);
+    $notify("VIP 修改成功", "", logStr);
     console.log(logStr);
     $done({ body: JSON.stringify(obj) });
   } else {
-    logStr += "响应体中未找到 {initState} 节点\n";
+    logStr += "未找到 {initState} 节点\n";
     $notify("脚本通知", "无目标节点", logStr);
     $done({ body });
   }
@@ -101,3 +95,4 @@ try {
   $notify("脚本错误", e.name, e.message);
   $done({ body });
 }
+
