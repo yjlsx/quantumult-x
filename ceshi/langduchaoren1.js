fc7a741836e61ae74a988b3bd277e8b5e5e87bf1
yjlsx <2413704955@qq.com>
2025-04-26 18:52:38 +0800

    Create langduchaoren1.js

---
+++ ceshi/langduchaoren1.js
@@ -0,0 +1,14 @@
+/**
+ * @supported BCFF6F0E-422A-495A-890E-0C63B9BC9F9A
+ */
+
+const newBody = {
+  "invitationcode": "D42476",
+  "user_id": "_5f8789a82656d528c315379063b0d40f",
+  "voiceType": "microsoft",
+  "speechCount": 999,
+  "chattype": "tts",
+  "messagetypes": []
+};
+
+$done({ body: newBody });
