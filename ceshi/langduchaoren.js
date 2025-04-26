f81dbfe841d631fdbc473dd1d8c63f3552943bea
yjlsx <2413704955@qq.com>
2025-04-26 19:18:52 +0800

    Update langduchaoren.js

--- ceshi/langduchaoren.js
+++ ceshi/langduchaoren.js
@@ -1,24 +1,20 @@
 /*
 [rewrite_local]
-# PhotoTalk 强制已被邀请，并改 databaseid
-^https:\/\/phototalk\.ivsapi\.com\/api\/user\/info script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/langduchaoren.js
-^https:\/\/phototalk\.ivsapi\.com\/api\/chat\/parameters\/v3\/$ url script-request-body_https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/langduchaoren1.js
 
-*
+^https:\/\/phototalk\.ivsapi\.com\/api\/user\/info$ script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/langduchaoren.js
+^https:\/\/phototalk\.ivsapi\.com\/api\/chat\/parameters\/v3\/$ url script-request-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/langduchaoren1.js
+
 [mitm]
 hostname = phototalk.ivsapi.com
-
 */
-
 
 let body = $response.body;
 let obj = JSON.parse(body);
-           
-obj.beinvited = 1;                               
-obj.user.updatems = 186954261428;   
-obj.user.databaseid = "us2";   
-                 
+
+obj.beinvited = 1;
+obj.user.updatems = 1864954261428;  
+obj.user.databaseid = "us2";
 
 body = JSON.stringify(obj);
-$done({body});
-
+$done({ body });
+                            
