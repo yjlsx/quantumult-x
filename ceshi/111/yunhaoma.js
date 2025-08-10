dbc2c34a7c00770305c5e7539cf692d5a3361f53
unknown <2413704955@qq.com>
2025-08-10 17:19:19 +0800

    Update yunhaoma.js

--- ceshi/111/yunhaoma.js
+++ ceshi/111/yunhaoma.js
@@ -2,11 +2,11 @@
 [rewrite]
 ^http:\/\/at\.kwedxef\.pro\/api\/v1\/sms\/user\/information url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
 ^http:\/\/at\.kwedxef\.pro\/api\/v1\/sms\/rent\/store url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
-^https:\/\/api\.smsvirtual\.app\/profile\/me$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
+^https:\/\/api\.smsvirtual\.app\/profile\/me url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
 
 *
 [mitm]
-hostname = at.kwedxef.pro, api.smsvirtual.app
+hostname =at.kwedxef.pro, api.smsvirtual.app
 
 */
 
@@ -19,29 +19,26 @@ hostname = at.kwedxef.pro, api.smsvirtual.app
     try {
       const obj = JSON.parse(body);
       if (obj && obj.data && typeof obj.data.coin !== 'undefined') {
-        obj.data.coin = 999999;
+        obj.data.coin = 999999; // 伪造金币数
       }
       body = JSON.stringify(obj);
-    } catch (e) {
-      // 解析失败，保持原始响应体
-    }
+    } catch (e) {}
   } else if (url.includes("/api/v1/sms/rent/store")) {
     body = JSON.stringify({
       status: 1,
-      data: "success",
+      data: "successs",
       code: 200
     });
   } else if (url.includes("/profile/me")) {
     try {
       const obj = JSON.parse(body);
-      if (obj && obj.data) {
-        obj.data.coin = 999999;
+      if (obj) {
         obj.is_subscriber = true;
         obj.subscriptions = [
           {
-            plan: "unlimited",
-            status: "active",
-            expiry_date: "2099-12-31T23:59:59Z"
+            "plan": "unlimited",
+            "status": "active",
+            "expiry_date": "2099-12-31T23:59:59Z"
           }
         ];
         obj.s_score = 999999;
@@ -49,9 +46,7 @@ hostname = at.kwedxef.pro, api.smsvirtual.app
         obj.balance = 999999;
       }
       body = JSON.stringify(obj);
-    } catch (e) {
-      // 解析失败，保持原始响应体
-    }
+    } catch (e) {}
   }
 
   $done({ body });
