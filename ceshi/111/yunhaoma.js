fafd32668a2eea89468b18352581902d97d5d131
unknown <2413704955@qq.com>
2025-08-10 17:11:52 +0800

    Update yunhaoma.js

--- ceshi/111/yunhaoma.js
+++ ceshi/111/yunhaoma.js
@@ -2,11 +2,11 @@
 [rewrite]
 ^http:\/\/at\.kwedxef\.pro\/api\/v1\/sms\/user\/information url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
 ^http:\/\/at\.kwedxef\.pro\/api\/v1\/sms\/rent\/store url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
-
+^https:\/\/api\.smsvirtual\.app\/profile\/me$ url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
 
 *
 [mitm]
-hostname = at.kwedxef.pro, appapi.51jobapp.com
+hostname = at.kwedxef.pro, api.smsvirtual.app
 
 */
 
@@ -19,18 +19,39 @@ hostname = at.kwedxef.pro, appapi.51jobapp.com
     try {
       const obj = JSON.parse(body);
       if (obj && obj.data && typeof obj.data.coin !== 'undefined') {
-        obj.data.coin = 999999; // <-- 如需其他数值，改这里
+        obj.data.coin = 999999;
       }
       body = JSON.stringify(obj);
     } catch (e) {
-      // JSON 解析失败，保持原响应不变
+      // 解析失败，保持原始响应体
     }
   } else if (url.includes("/api/v1/sms/rent/store")) {
     body = JSON.stringify({
       status: 1,
-      data: "successs",
+      data: "success",
       code: 200
     });
+  } else if (url.includes("/profile/me")) {
+    try {
+      const obj = JSON.parse(body);
+      if (obj && obj.data) {
+        obj.data.coin = 999999;
+        obj.is_subscriber = true;
+        obj.subscriptions = [
+          {
+            plan: "unlimited",
+            status: "active",
+            expiry_date: "2099-12-31T23:59:59Z"
+          }
+        ];
+        obj.s_score = 999999;
+        obj.subscription_balance = 999999;
+        obj.balance = 999999;
+      }
+      body = JSON.stringify(obj);
+    } catch (e) {
+      // 解析失败，保持原始响应体
+    }
   }
 
   $done({ body });
