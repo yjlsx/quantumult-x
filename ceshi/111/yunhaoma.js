c69d84d53634aadd8e28a5b27b66aff8cb603aff
unknown <2413704955@qq.com>
2025-08-10 16:54:01 +0800

    Update yunhaoma.js

--- ceshi/111/yunhaoma.js
+++ ceshi/111/yunhaoma.js
@@ -1,6 +1,7 @@
 /*
 [rewrite]
 ^http:\/\/at\.kwedxef\.pro\/api\/v1\/sms\/user\/information url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
+^http:\/\/at\.kwedxef\.pro\/api\/v1\/sms\/rent\/store url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/yunhaoma.js
 
 
 *
@@ -11,18 +12,26 @@ hostname = at.kwedxef.pro, appapi.51jobapp.com
 
 
 (function() {
+  const url = $request.url;
   let body = $response.body;
-  let obj;
-  try {
-    obj = JSON.parse(body);
-  } catch (e) {
-    $done({ body });
-    return;
+
+  if (url.includes("/api/v1/sms/user/information")) {
+    try {
+      const obj = JSON.parse(body);
+      if (obj && obj.data && typeof obj.data.coin !== 'undefined') {
+        obj.data.coin = 999999; // <-- 如需其他数值，改这里
+      }
+      body = JSON.stringify(obj);
+    } catch (e) {
+      // JSON 解析失败，保持原响应不变
+    }
+  } else if (url.includes("/api/v1/sms/rent/store")) {
+    body = JSON.stringify({
+      status: 1,
+      data: "successs",
+      code: 200
+    });
   }
 
-  if (obj && obj.data && typeof obj.data.coin !== 'undefined') {
-    obj.data.coin = 999999; // <-- 这里改成你想要的数值
-  }
-
-  $done({ body: JSON.stringify(obj) });
+  $done({ body });
 })();
