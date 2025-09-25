77a8d07da98b0afe9774f1e4fdad52628ab7cba8
yjlsx <2413704955@qq.com>
2025-09-25 19:49:51 +0800

    Update zhilian.js

--- ceshi/zhilian.js
+++ ceshi/zhilian.js
@@ -10,7 +10,7 @@
 ^https://m\.zhaopin\.com/bapi/vip/privilege/info url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/zhilian.js
 ^https:\/\/m\.zhaopin\.com\/bapi\/vip\/buy\/gray url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/zhilian.js
 ^https:\/\/pro\.bugly\.qq\.com\/v1 url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/111/zhilian1.js
-^https:\/\/m\zhaopin\.com\/bapi\/order\/prepay-creation url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/zhilian.js
+^https:\/\/m\.zhaopin\.com\/bapi\/order\/prepay-creation url script-response-body https://raw.githubusercontent.com/yjlsx/quantumult-x/master/ceshi/zhilian.js
 
 
 
